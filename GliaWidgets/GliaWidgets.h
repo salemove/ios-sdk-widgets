8e67fb6abd81f18e8613a3f4a189255126528a8a
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2020-11-18 15:44:19 +0200

    Initial Commit

---
+++ GliaWidgets/GliaWidgets.h
@@ -0,0 +1,18 @@
+//
+//  GliaWidgets.h
+//  GliaWidgets
+//
+//  Created by Eigo on 18.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GliaWidgets.
+FOUNDATION_EXPORT double GliaWidgetsVersionNumber;
+
+//! Project version string for GliaWidgets.
+FOUNDATION_EXPORT const unsigned char GliaWidgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GliaWidgets/PublicHeader.h>
+
+
