973365eed0da398fecf8bce4044aabb0aa78e69c
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2021-01-05 14:13:34 +0200

    load and store queue

--- GliaWidgets/ViewModel/Chat/Storage/ChatStorage.swift
+++ GliaWidgets/ViewModel/Chat/Storage/ChatStorage.swift
@@ -2,12 +2,12 @@ import SQLite3
 
 class ChatStorage {
     struct Queue {
-        let id: Int32
+        let id: Int64
         let queueID: String
     }
 
     struct Operator {
-        let id: Int32
+        let id: Int64
         let name: String
         let pictureUrl: String?
     }
@@ -18,9 +18,10 @@ class ChatStorage {
             case `operator` = 1
         }
 
-        let id: String
-        let queueID: Int32
-        let operatorID: Int32?
+        let id: Int64
+        let messageID: String
+        let queueID: Int
+        let operatorID: Int?
         let sender: Sender
         let content: String
         let timestamp: Int64
@@ -32,6 +33,7 @@ class ChatStorage {
     private var db: OpaquePointer?
     private let dbURL: URL?
     private let kDBName = "GliaChat.sqlite"
+    private var lastInsertedRowID: Int64 { return sqlite3_last_insert_rowid(db) }
 
     init() {
         dbURL = try? FileManager.default
@@ -39,6 +41,10 @@ class ChatStorage {
             .appendingPathComponent(kDBName)
         openDatabase()
         createTables()
+    }
+
+    deinit {
+        sqlite3_close(db)
     }
 
     private func openDatabase() {
@@ -49,18 +55,18 @@ class ChatStorage {
     private func createTables() {
         let queueTableSQL = """
             CREATE TABLE IF NOT EXISTS Queue(
-            id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY NOT NULL,
             queueID TEXT NOT NULL);
         """
         let operatorTableSQL = """
             CREATE TABLE IF NOT EXISTS Operator(
-            id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY NOT NULL,
             name TEXT NOT NULL,
             pictureUrl TEXT);
         """
         let messagesTableSQL = """
             CREATE TABLE IF NOT EXISTS Message(
-            id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY NOT NULL,
             messageID STRING NOT NULL,
             queueID INTEGER NOT NULL,
             operatorID INTEGER,
@@ -70,9 +76,13 @@ class ChatStorage {
             FOREIGN KEY(queueID) REFERENCES Queue(id),
             FOREIGN KEY(operatorID) REFERENCES Operator(id));
         """
+        let queueIDIndex = """
+            CREATE UNIQUE INDEX index_queueid ON Queue(queueID);
+        """
         execSQL(queueTableSQL)
         execSQL(operatorTableSQL)
         execSQL(messagesTableSQL)
+        execSQL(queueIDIndex)
     }
 
     private func execSQL(_ sql: String) {
@@ -81,5 +91,52 @@ class ChatStorage {
             sqlite3_step(statement)
         }
         sqlite3_finalize(statement)
+    }
+}
+
+extension ChatStorage {
+    func setQueue(withID queueID: String) {
+        if let storedQueue = loadQueue(withID: queueID) {
+            queue = storedQueue
+        } else {
+            queue = insertQueue(withID: queueID)
+        }
+    }
+
+    private func loadQueue(withID queueID: String) -> Queue? {
+        let sql = "SELECT id, queueID FROM Queue WHERE QueueID = '\(queueID)';"
+        var statement: OpaquePointer?
+
+        if sqlite3_prepare_v2(db, sql, -1, &statement, nil) == SQLITE_OK {
+            if sqlite3_step(statement) == SQLITE_ROW {
+                let id = sqlite3_column_int64(statement, 0)
+                let queueID = String(describing: String(cString: sqlite3_column_text(statement, 1)))
+                return Queue(id: id, queueID: queueID)
+            }
+        }
+
+        sqlite3_finalize(statement)
+        
+        return nil
+    }
+
+    private func insertQueue(withID queueID: String) -> Queue? {
+        let sql = "INSERT INTO Queue(QueueID) VALUES (?);"
+        var statement: OpaquePointer?
+
+        if sqlite3_prepare_v2(db, sql, -1, &statement, nil) == SQLITE_OK {
+            sqlite3_bind_text(statement, 1, (queueID as NSString).utf8String, -1, nil)
+
+            if sqlite3_step(statement) == SQLITE_DONE {
+                return Queue(id: lastInsertedRowID,
+                             queueID: queueID)
+            } else {
+                return nil
+            }
+        }
+
+        sqlite3_finalize(statement)
+
+        return nil
     }
 }
