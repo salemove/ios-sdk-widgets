5aa7f16bd3441d5641c5f63d82b80524de25116b
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2021-01-06 11:53:25 +0200

    SQLite error handling

--- GliaWidgets/ViewModel/Chat/Storage/ChatStorage.swift
+++ GliaWidgets/ViewModel/Chat/Storage/ChatStorage.swift
@@ -20,46 +20,59 @@ class ChatStorage {
 
         let id: Int64
         let messageID: String
-        let queueID: Int
-        let operatorID: Int?
+        let queueID: Int64
+        let operatorID: Int64?
         let sender: Sender
         let content: String
         let timestamp: Int64
     }
 
-    private enum Result {
-        case success(OpaquePointer?)
-        case failure
+    private enum SQLiteError: Error {
+        case openDatabase
+        case prepare
+        case insert
     }
+
+    var storedMessages: [Message] { return messages }
 
     private var queue: Queue? {
         didSet { loadMessages() }
     }
     private var `operator`: Operator?
     private var messages = [Message]()
+    private var operatorCache = [Int64: Operator]()
     private var db: OpaquePointer?
     private let dbURL: URL?
     private let kDBName = "GliaChat.sqlite"
     private var lastInsertedRowID: Int64 { return sqlite3_last_insert_rowid(db) }
+    private var lastErrorMessage: String { return sqlite3_errmsg(db).map({ String(cString: $0) }) ?? "UNKNOWN" }
 
     init() {
         dbURL = try? FileManager.default
             .url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
             .appendingPathComponent(kDBName)
-        openDatabase()
-        createTables()
+
+        do {
+            try openDatabase()
+            try createTables()
+        } catch {
+            print("\(#function): \(lastErrorMessage)")
+        }
     }
 
     deinit {
         sqlite3_close(db)
     }
 
-    private func openDatabase() {
+    private func openDatabase() throws {
         guard let dbPath = dbURL?.path else { return }
-        sqlite3_open(dbPath, &db)
+
+        if sqlite3_open(dbPath, &db) != SQLITE_OK {
+            throw SQLiteError.openDatabase
+        }
     }
 
-    private func createTables() {
+    private func createTables() throws {
         let queueTableSQL = """
             CREATE TABLE IF NOT EXISTS Queue(
             id INTEGER PRIMARY KEY NOT NULL,
@@ -86,133 +99,137 @@ class ChatStorage {
         let queueIDIndex = """
             CREATE UNIQUE INDEX index_queueid ON Queue(queueID);
         """
-        exec(queueTableSQL)
-        exec(operatorTableSQL)
-        exec(messagesTableSQL)
-        exec(queueIDIndex)
+
+        try exec(queueTableSQL)
+        try exec(operatorTableSQL)
+        try exec(messagesTableSQL)
+        try exec(queueIDIndex)
     }
 
-    private func exec(_ sql: String, completion: ((Result) -> Void)? = nil) {
-        prepare(sql) {
-            switch $0 {
-            case .success(let statement):
-                sqlite3_step(statement)
-                completion?(.success(statement))
-            case .failure:
-                completion?(.failure)
-            }
+    private func exec(_ sql: String, completion: (() -> Void)? = nil) throws {
+        try prepare(sql) {
+            sqlite3_step($0)
+            completion?()
         }
     }
 
-    private func insert(_ sql: String, values: [Any?], completion: (Result) -> Void) {
-        prepare(sql) {
-            switch $0 {
-            case .success(let statement):
-                values.enumerated().forEach({
-                    let index = Int32($0.offset + 1)
-                    switch $0.element {
-                    case nil:
-                        sqlite3_bind_null(statement, index)
-                    case let value as Int32:
-                        sqlite3_bind_int(statement, index, value)
-                    case let value as Int64:
-                        sqlite3_bind_int64(statement, index, value)
-                    case let value as String:
-                        sqlite3_bind_text(statement, index, (value as NSString).utf8String, -1, nil)
-                    default:
-                        break
-                    }
-                })
-
-                if sqlite3_step(statement) == SQLITE_DONE {
-                    completion(.success(statement))
-                } else {
-                    completion(.failure)
+    private func insert(_ sql: String, values: [Any?], completion: () -> Void) throws {
+        try prepare(sql) { statement in
+            values.enumerated().forEach({
+                let index = Int32($0.offset + 1)
+                switch $0.element {
+                case nil:
+                    sqlite3_bind_null(statement, index)
+                case let value as Int32:
+                    sqlite3_bind_int(statement, index, value)
+                case let value as Int64:
+                    sqlite3_bind_int64(statement, index, value)
+                case let value as String:
+                    sqlite3_bind_text(statement, index, (value as NSString).utf8String, -1, nil)
+                default:
+                    break
                 }
-            case .failure:
-                completion(.failure)
+            })
+
+            if sqlite3_step(statement) == SQLITE_DONE {
+                completion()
+            } else {
+                throw SQLiteError.insert
             }
         }
     }
 
-    private func prepare(_ sql: String, completion: (Result) -> Void) {
+    private func prepare(_ sql: String, work: (OpaquePointer) throws -> Void) throws {
         var statement: OpaquePointer?
-        if sqlite3_prepare_v2(db, sql, -1, &statement, nil) == SQLITE_OK {
-            completion(.success(statement))
-        } else {
-            completion(.failure)
+
+        defer {
+            sqlite3_finalize(statement)
         }
-        sqlite3_finalize(statement)
+
+        if sqlite3_prepare_v2(db, sql, -1, &statement, nil) == SQLITE_OK, let statement = statement {
+            try work(statement)
+        } else {
+            throw SQLiteError.prepare
+        }
     }
 }
 
 extension ChatStorage {
     func setQueue(withID queueID: String) {
-        loadQueue(withID: queueID) { queue in
-            if let queue = queue {
-                self.queue = queue
-            } else {
-                insertQueue(withID: queueID) { queue in
+        do {
+            try loadQueue(withID: queueID) { queue in
+                if let queue = queue {
                     self.queue = queue
-                }
-            }
-        }
-    }
-
-    private func loadQueue(withID queueID: String, completion: (Queue?) -> Void) {
-        prepare("SELECT id, queueID FROM Queue WHERE queueID = '\(queueID)';") {
-            switch $0 {
-            case .success(let statement):
-                if sqlite3_step(statement) == SQLITE_ROW {
-                    let id = sqlite3_column_int64(statement, 0)
-                    let queueID = String(describing: String(cString: sqlite3_column_text(statement, 1)))
-                    let queue = Queue(id: id, queueID: queueID)
-                    completion(queue)
                 } else {
-                    completion(nil)
+                    try insertQueue(withID: queueID) { queue in
+                        self.queue = queue
+                    }
                 }
-            case .failure:
-                completion(nil)
+            }
+        } catch {
+            print("\(#function): \(lastErrorMessage)")
+        }
+    }
+
+    private func loadQueue(withID queueID: String, completion: (Queue?) throws -> Void) throws {
+        try prepare("SELECT id, queueID FROM Queue WHERE queueID = '\(queueID)';") {
+            if sqlite3_step($0) == SQLITE_ROW {
+                let id = sqlite3_column_int64($0, 0)
+                let queueID = String(cString: sqlite3_column_text($0, 1))
+                let queue = Queue(id: id, queueID: queueID)
+                try completion(queue)
+            } else {
+                try completion(nil)
             }
         }
     }
 
-    private func insertQueue(withID queueID: String, completion: (Queue?) -> Void) {
-        insert("INSERT INTO Queue(queueID) VALUES (?);",
-               values: [queueID]) {
-            switch $0 {
-            case .success:
-                completion(Queue(id: lastInsertedRowID,
-                                 queueID: queueID))
-            case .failure:
-                completion(nil)
-            }
+    private func insertQueue(withID queueID: String, completion: (Queue?) -> Void) throws {
+        try insert("INSERT INTO Queue(queueID) VALUES (?);", values: [queueID]) {
+            completion(Queue(id: lastInsertedRowID,
+                             queueID: queueID))
         }
     }
 }
 
 extension ChatStorage {
     func setOperator(name: String, pictureUrl: String?) {
-        insertOperator(name: name, pictureUrl: pictureUrl) {
-            self.operator = $0
+        do {
+            try insertOperator(name: name, pictureUrl: pictureUrl) {
+                self.operator = $0
+            }
+        } catch {
+            print("\(#function): \(lastErrorMessage)")
         }
     }
 
     func storedOperator(withID id: Int64) -> Operator? {
-        return nil // TODO
+        if let storedOperator = operatorCache[id] {
+            return storedOperator
+        } else {
+            var storedOperator: Operator?
+            do {
+                try prepare("SELECT id, name, pictureUrl FROM Operator WHERE id = '\(id)';") {
+                    if sqlite3_step($0) == SQLITE_ROW {
+                        let id = sqlite3_column_int64($0, 0)
+                        let name = String(cString: sqlite3_column_text($0, 1))
+                        let pictureUrl = sqlite3_column_text($0, 1).map({ String(cString: $0) })
+                        storedOperator = Operator(id: id, name: name, pictureUrl: pictureUrl)
+                    }
+                }
+            } catch {
+                print("\(#function): \(lastErrorMessage)")
+            }
+
+            return storedOperator
+        }
     }
 
-    private func insertOperator(name: String, pictureUrl: String?, completion: (Operator?) -> Void) {
-        insert("INSERT INTO Operator(name, pictureUrl) VALUES (?,?);",
-               values: [name, pictureUrl]) {
-            switch $0 {
-            case .success:
-                completion(Operator(id: lastInsertedRowID,
-                                    name: name,
-                                    pictureUrl: pictureUrl))
-            case .failure:
-                completion(nil)
-            }
+    private func insertOperator(name: String, pictureUrl: String?, completion: (Operator?) -> Void) throws {
+        try insert("INSERT INTO Operator(name, pictureUrl) VALUES (?,?);", values: [name, pictureUrl]) {
+            completion(Operator(id: lastInsertedRowID,
+                                name: name,
+                                pictureUrl: pictureUrl))
         }
     }
 }
