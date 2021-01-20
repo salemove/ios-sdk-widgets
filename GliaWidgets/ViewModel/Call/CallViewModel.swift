e9406d9d9b66cff7886d9206e3b922f5d5ae2efc
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2021-01-20 10:35:36 +0200

    refactoring

--- GliaWidgets/ViewModel/Call/CallViewModel.swift
+++ GliaWidgets/ViewModel/Call/CallViewModel.swift
@@ -3,26 +3,15 @@ import SalemoveSDK
 class CallViewModel: EngagementViewModel, ViewModel {
     enum Event {
         case viewDidLoad
-        case backTapped
-        case closeTapped
     }
 
     enum Action {
         case queueWaiting
         case queueConnecting
         case queueConnected(name: String?, imageUrl: String?)
-        case showEndButton
-        case confirm(ConfirmationAlertConf,
-                     confirmed: (() -> Void)?)
-        case showAlert(MessageAlertConf,
-                       dismissed: (() -> Void)?)
     }
 
-    enum DelegateEvent {
-        case back
-        case operatorImage(url: String?)
-        case finished
-    }
+    enum DelegateEvent {}
 
     var action: ((Action) -> Void)?
     var delegate: ((DelegateEvent) -> Void)?
@@ -35,10 +24,6 @@ class CallViewModel: EngagementViewModel, ViewModel {
         switch event {
         case .viewDidLoad:
             start()
-        case .backTapped:
-            delegate?(.back)
-        case .closeTapped:
-            closeTapped()
         }
     }
 
@@ -46,71 +31,9 @@ class CallViewModel: EngagementViewModel, ViewModel {
         //enqueue()
     }
 
-    private func enqueue() {
-        interactor.enqueueForEngagement {
-
-        } failure: { error in
-            self.handleError(error)
-        }
-    }
-
-    private func end() {
-        interactor.endSession {
-            self.delegate?(.finished)
-        } failure: { _ in
-            self.delegate?(.finished)
-        }
-    }
-
-    private func closeTapped() {
-        switch interactor.state {
-        case .enqueueing, .enqueued:
-            action?(.confirm(alertConf.leaveQueue,
-                             confirmed: { self.end() }))
-        case .engaged:
-            action?(.confirm(alertConf.endEngagement,
-                             confirmed: { self.end() }))
-        default:
-            end()
-        }
-    }
-
-    private func handleError(_ error: SalemoveError) {
-        switch error.error {
-        case let queueError as QueueError:
-            switch queueError {
-            case .queueClosed, .queueFull:
-                self.showAlert(with: self.alertConf.operatorsUnavailable,
-                               dismissed: { self.end() })
-            default:
-                self.showAlert(with: self.alertConf.unexpectedError,
-                               dismissed: { self.end() })
-            }
-        default:
-            self.showAlert(with: self.alertConf.unexpectedError,
-                           dismissed: { self.end() })
-        }
-    }
-
-    private func showAlert(with conf: MessageAlertConf, dismissed: (() -> Void)?) {
-        let dismissHandler = {
-            self.alertState = .none
-            dismissed?()
-
-            switch self.interactor.state {
-            case .inactive:
-                self.end()
-            default:
-                break
-            }
-        }
-
-        alertState = .presenting
-
-        action?(.showAlert(conf, dismissed: { dismissHandler() }))
-    }
-
     override func interactorEvent(_ event: InteractorEvent) {
+        super.interactorEvent(event)
+
         switch event {
         case .stateChanged(let state):
             switch state {
@@ -129,10 +52,6 @@ class CallViewModel: EngagementViewModel, ViewModel {
                 action?(.showEndButton)
                 delegate?(.operatorImage(url: engagedOperator?.picture?.url))
             }
-        case .upgradeOffer(let offer, answer: let answer):
-            break
-        case .error(let error):
-            self.handleError(error)
         default:
             break
         }
--- GliaWidgets/ViewModel/EngagementViewModel.swift
+++ GliaWidgets/ViewModel/EngagementViewModel.swift
@@ -1,10 +1,32 @@
 import SalemoveSDK
 
 class EngagementViewModel {
+    enum Event {
+        case backTapped
+        case closeTapped
+    }
+
+    enum Action {
+        case showEndButton
+        case confirm(ConfirmationAlertConf,
+                     confirmed: (() -> Void)?)
+        case showAlert(MessageAlertConf,
+                       dismissed: (() -> Void)?)
+    }
+
+    enum DelegateEvent {
+        case back
+        case operatorImage(url: String?)
+        case finished
+    }
+
     enum AlertState {
         case presenting
         case none
     }
+
+    var engagementAction: ((Action) -> Void)?
+    var engagementDelegate: ((DelegateEvent) -> Void)?
 
     let interactor: Interactor
     let alertConf: AlertConf
@@ -21,10 +43,102 @@ class EngagementViewModel {
         interactor.removeObserver(self)
     }
 
-    func interactorEvent(_ event: InteractorEvent) {}
+    public func event(_ event: Event) {
+        switch event {
+        case .backTapped:
+            engagementDelegate?(.back)
+        case .closeTapped:
+            closeTapped()
+        }
+    }
+
+    func enqueue() {
+        interactor.enqueueForEngagement {
+
+        } failure: { error in
+            self.handleError(error)
+        }
+    }
+
+    private func endEngagement() {
+        interactor.endSession {
+            self.engagementDelegate?(.finished)
+        } failure: { _ in
+            self.engagementDelegate?(.finished)
+        }
+    }
+
+    private func closeTapped() {
+        switch interactor.state {
+        case .enqueueing, .enqueued:
+            engagementAction?(.confirm(alertConf.leaveQueue,
+                                       confirmed: { self.endEngagement() }))
+        case .engaged:
+            engagementAction?(.confirm(alertConf.endEngagement,
+                                       confirmed: { self.endEngagement() }))
+        default:
+            endEngagement()
+        }
+    }
+
+    private func handleError(_ error: SalemoveError) {
+        switch error.error {
+        case let queueError as QueueError:
+            switch queueError {
+            case .queueClosed, .queueFull:
+                self.showAlert(with: self.alertConf.operatorsUnavailable,
+                               dismissed: { self.endEngagement() })
+            default:
+                self.showAlert(with: self.alertConf.unexpectedError,
+                               dismissed: { self.endEngagement() })
+            }
+        default:
+            self.showAlert(with: self.alertConf.unexpectedError,
+                           dismissed: { self.endEngagement() })
+        }
+    }
+
+    private func showAlert(with conf: MessageAlertConf, dismissed: (() -> Void)?) {
+        let dismissHandler = {
+            self.alertState = .none
+            dismissed?()
+
+            switch self.interactor.state {
+            case .inactive:
+                self.endEngagement()
+            default:
+                break
+            }
+        }
+
+        alertState = .presenting
+
+        engagementAction?(.showAlert(conf, dismissed: { dismissHandler() }))
+    }
 
     func alertConf(with error: SalemoveError) -> MessageAlertConf {
         return MessageAlertConf(with: error,
                                 templateConf: self.alertConf.apiError)
+    }
+
+    func interactorEvent(_ event: InteractorEvent) {
+        switch event {
+        case .stateChanged(let state):
+            switch state {
+            case .inactive:
+                if alertState == .none {
+                    engagementDelegate?(.finished)
+                }
+            case .engaged(let engagedOperator):
+                engagementAction?(.showEndButton)
+                engagementDelegate?(.operatorImage(url: engagedOperator?.picture?.url))
+            default:
+                break
+            }
+        case .error(let error):
+            self.handleError(error)
+        default:
+            break
+        }
     }
 }
