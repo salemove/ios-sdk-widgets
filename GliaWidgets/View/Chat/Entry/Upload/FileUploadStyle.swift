062b402a6b2df05d358b8c0578cdd9971e685d3e
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2021-04-07 11:57:01 +0300

    docs update

--- GliaWidgets/View/Chat/Entry/Upload/FileUploadStyle.swift
+++ GliaWidgets/View/Chat/Entry/Upload/FileUploadStyle.swift
@@ -1,16 +1,46 @@
 import UIKit
 
+/// Style of a single upload view in the uploads list view.
 public class FileUploadStyle {
+    /// Style of the file's image.
     public var fileImage: FileImageStyle
+
+    /// Style of the uploading state.
     public var uploading: FileUploadStateStyle
+
+    /// Style of the uploaded state.
     public var uploaded: FileUploadStateStyle
+
+    /// Style of the error state.
     public var error: FileUploadErrorStateStyle
+
+    /// Foreground color of the upload progress bar.
     public var progressColor: UIColor
+
+    /// Foreground color of the upload progress bar in error state.
     public var errorProgressColor: UIColor
+
+    /// Background color of the upload progress bar.
     public var progressBackgroundColor: UIColor
+
+    /// Image of the remove button.
     public var removeButtonImage: UIImage
+
+    /// Color of the remove button image.
     public var removeButtonColor: UIColor
 
+    ///
+    /// - Parameters:
+    ///   - fileImage: Style of the file's image.
+    ///   - uploading: Style of the uploading state.
+    ///   - uploaded: Style of the uploaded state.
+    ///   - error: Style of the error state.
+    ///   - progressColor: Foreground color of the upload progress bar.
+    ///   - errorProgressColor: Foreground color of the upload progress bar in error state.
+    ///   - progressBackgroundColor: Background color of the upload progress bar.
+    ///   - removeButtonImage: Image of the remove button.
+    ///   - removeButtonColor: Color of the remove button image.
+    ///
     public init(fileImage: FileImageStyle,
                 uploading: FileUploadStateStyle,
                 uploaded: FileUploadStateStyle,
@@ -32,13 +62,31 @@ public class FileUploadStyle {
     }
 }
 
+/// Style of an upload state.
 public class FileUploadStateStyle {
+    /// Text for the state.
     public var text: String
+
+    /// Font of the state text.
     public var font: UIFont
+
+    /// Color of the state text.
     public var textColor: UIColor
+
+    /// Font of the file info information text.
     public var infoFont: UIFont
+
+    /// Color of the file information text.
     public var infoColor: UIColor
 
+    ///
+    /// - Parameters:
+    ///   - text: Text for the state.
+    ///   - font: Font of the state text.
+    ///   - textColor: Color of the state text.
+    ///   - infoFont: Font of the file info information text.
+    ///   - infoColor: Color of the file information text.
+    ///
     public init(text: String,
                 font: UIFont,
                 textColor: UIColor,
@@ -52,18 +100,51 @@ public class FileUploadStateStyle {
     }
 }
 
+/// Style of an upload error state.
 public class FileUploadErrorStateStyle {
+    /// Text for the state.
     public var text: String
+
+    /// Font of the state text.
     public var font: UIFont
+
+    /// Color of the state text.
     public var textColor: UIColor
+
+    /// Font of the information text.
     public var infoFont: UIFont
+
+    /// Color of the information text.
     public var infoColor: UIColor
+
+    /// Information text to display when selected file is too big.
     public var infoFileTooBig: String
+
+    /// Information text to display when selected file's type is not supported.
     public var infoUnsupportedFileType: String
+
+    /// Information text to display when selected file's safety check failed.
     public var infoSafetyCheckFailed: String
+
+    /// Information text to display on network related error.
     public var infoNetworkError: String
+
+    /// Information text to display on generic error.
     public var infoGenericError: String
 
+    ///
+    /// - Parameters:
+    ///   - text: Text for the state.
+    ///   - font: Font of the state text.
+    ///   - textColor: Color of the state text.
+    ///   - infoFont: Font of the information text.
+    ///   - infoColor: Color of the information text.
+    ///   - infoFileTooBig: Information text to display when selected file is too big.
+    ///   - infoUnsupportedFileType: Information text to display when selected file's type is not supported.
+    ///   - infoSafetyCheckFailed: Information text to display when selected file's safety check failed.
+    ///   - infoNetworkError: Information text to display on network related error.
+    ///   - infoGenericError: Information text to display on generic error.
+    ///
     public init(text: String,
                 font: UIFont,
                 textColor: UIColor,
--- GliaWidgets/View/Chat/Upgrade/ChatCallUpgradeStyle.swift
+++ GliaWidgets/View/Chat/Upgrade/ChatCallUpgradeStyle.swift
@@ -1,15 +1,42 @@
 import UIKit
 
+/// Style of the call upgrade view. Shown after engagement is upgraded to a call.
 public class ChatCallUpgradeStyle {
+    /// An icon indicating upgraded engagement kind.
     public var icon: UIImage
+
+    /// Color of the icon image.
     public var iconColor: UIColor
+
+    /// Text to display in the view.
     public var text: String
+
+    /// Font of the text.
     public var textFont: UIFont
+
+    /// Color of the text.
     public var textColor: UIColor
+
+    /// Font of the duration counter text.
     public var durationFont: UIFont
+
+    /// Color of the duration counter text.
     public var durationColor: UIColor
+
+    /// Border color of the view.
     public var borderColor: UIColor
 
+    ///
+    /// - Parameters:
+    ///   - icon: An icon indicating upgraded engagement kind.
+    ///   - iconColor: Color of the icon image.
+    ///   - text: Text to display in the view.
+    ///   - textFont: Font of the text.
+    ///   - textColor: Color of the text.
+    ///   - durationFont: Font of the duration counter text.
+    ///   - durationColor: Color of the duration counter text.
+    ///   - borderColor: Border color of the view.
+    ///
     public init(icon: UIImage,
                 iconColor: UIColor,
                 text: String,
