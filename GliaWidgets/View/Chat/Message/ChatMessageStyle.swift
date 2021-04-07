c1d8ad004082c3bd70444366ff239ea60e5d5636
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2021-04-07 12:49:13 +0300

    chat documentation

--- GliaWidgets/View/Chat/Message/ChatMessageStyle.swift
+++ GliaWidgets/View/Chat/Message/ChatMessageStyle.swift
@@ -1,10 +1,22 @@
 import UIKit
 
+/// Base style of a chat message.
 public class ChatMessageStyle {
+    /// Style of a text content.
     public var text: ChatTextContentStyle
+
+    /// Style of a image content.
     public var imageFile: ChatImageFileContentStyle
+
+    /// Style of a downloadable file content.
     public var fileDownload: ChatFileDownloadStyle
 
+    ///
+    /// - Parameters:
+    ///   - text: Style of a text content.
+    ///   - imageFile: Style of a image content.
+    ///   - fileDownload: Style of a downloadable file content.
+    ///
     public init(text: ChatTextContentStyle,
                 imageFile: ChatImageFileContentStyle,
                 fileDownload: ChatFileDownloadStyle) {
--- GliaWidgets/View/Chat/Message/Content/Text/ChatTextContentStyle.swift
+++ GliaWidgets/View/Chat/Message/Content/Text/ChatTextContentStyle.swift
@@ -1,10 +1,22 @@
 import UIKit
 
+/// Style of a text message.
 public class ChatTextContentStyle {
+    /// Font of the message text.
     public var textFont: UIFont
+
+    /// Color of the message text.
     public var textColor: UIColor
+
+    /// Background color of the view.
     public var backgroundColor: UIColor
 
+    ///
+    /// - Parameters:
+    ///   - textFont: Font of the message text.
+    ///   - textColor: Color of the message text.
+    ///   - backgroundColor: Background color of the content view.
+    ///
     public init(textFont: UIFont,
                 textColor: UIColor,
                 backgroundColor: UIColor) {
