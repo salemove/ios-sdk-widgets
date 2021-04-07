3d128200c9539127d7712e1748b29418c6bdb134
Eigo Madaloja <eigo.madaloja@mooncascade.com>
2021-04-07 13:16:17 +0300

    call documentation

--- GliaWidgets/View/Common/Alert/AlertStyle.swift
+++ GliaWidgets/View/Common/Alert/AlertStyle.swift
@@ -1,16 +1,46 @@
 import UIKit
 
+/// Style of an alert view.
 public struct AlertStyle {
+    /// Font of the title text.
     public var titleFont: UIFont
+
+    /// Color of the title text.
     public var titleColor: UIColor
+
+    /// Color of the title image.
     public var titleImageColor: UIColor
+
+    /// Font of the message text.
     public var messageFont: UIFont
+
+    /// Color of the message text.
     public var messageColor: UIColor
+
+    /// Background color of the view.
     public var backgroundColor: UIColor
+
+    /// Color of the close button.
     public var closeButtonColor: UIColor
+
+    /// Style of a positive action button.
     public var positiveAction: ActionButtonStyle
+
+    /// Style of a negative action button.
     public var negativeAction: ActionButtonStyle
 
+    ///
+    /// - Parameters:
+    ///   - titleFont: Font of the title text.
+    ///   - titleColor: Color of the title text.
+    ///   - titleImageColor: Color of the title image.
+    ///   - messageFont: Font of the message text.
+    ///   - messageColor: Color of the message text.
+    ///   - backgroundColor: Background color of the view.
+    ///   - closeButtonColor: Color of the close button.
+    ///   - positiveAction: Style of a positive action button.
+    ///   - negativeAction: Style of a negative action button.
+    ///
     public init(titleFont: UIFont,
                 titleColor: UIColor,
                 titleImageColor: UIColor,
