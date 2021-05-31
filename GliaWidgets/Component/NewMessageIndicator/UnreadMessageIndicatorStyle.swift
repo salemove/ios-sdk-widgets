0d278a06f2c93e8577365767e4982dfcf0376e77
Vladyslav Kupriienko <78469009+VladKupriienko@users.noreply.github.com>
2021-05-31 15:16:49 +0300

    [MUIC-279] [MUIC-281] New message indicator (#53)

---
+++ GliaWidgets/Component/NewMessageIndicator/UnreadMessageIndicatorStyle.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+public struct UnreadMessageIndicatorStyle {
+    public var badge: BadgeStyle
+    public var userImage: UserImageStyle
+
+    public init(
+        badgeFont: UIFont,
+        badgeTextColor: UIColor,
+        badgeColor: UIColor,
+        placeholderImage: UIImage?,
+        placeholderColor: UIColor,
+        placeholderBackgroundColor: UIColor,
+        imageBackgroundColor: UIColor
+    ) {
+        self.badge = BadgeStyle(
+            font: badgeFont,
+            fontColor: badgeTextColor,
+            backgroundColor: badgeColor
+        )
+        self.userImage = UserImageStyle(
+            placeholderImage: placeholderImage,
+            placeholderColor: placeholderColor,
+            placeholderBackgroundColor: placeholderBackgroundColor,
+            imageBackgroundColor: imageBackgroundColor
+        )
+    }
+}
