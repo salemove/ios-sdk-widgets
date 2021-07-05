5b41328afb22226d68018d0db2924f85a5fbd71a
Vlad Kupriienko <vladyslav.kupriienko@mooncascade.com>
2021-07-05 18:51:51 +0300

    [MUIC-309] Fix local video view sometimes not appearing

--- GliaWidgets/View/Call/Video/VideoStreamView.swift
+++ GliaWidgets/View/Call/Video/VideoStreamView.swift
@@ -16,6 +16,7 @@ class VideoStreamView: View {
                 streamView.scale = .aspectFill
                 addSubview(streamView)
                 streamView.autoPinEdgesToSuperviewEdges()
+                streamView.layoutIfNeeded()
             }
         }
     }
