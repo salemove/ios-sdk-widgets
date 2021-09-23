2e4cc0c5d50c17ba7615f09acdd2fcbee90c5f7d
Karina Karimova <karimovakarina180@gmail.com>
2021-09-23 16:13:58 +0300

    Improve readability

--- GliaWidgets/View/Chat/Message/ChoiceCardView.swift
+++ GliaWidgets/View/Chat/Message/ChoiceCardView.swift
@@ -1,17 +1,22 @@
 import UIKit
+
 final class ChoiceCardView: OperatorChatMessageView {
     var onOptionTapped: ((ChatChoiceCardOption) -> Void)!
+
     private let viewStyle: ChoiceCardStyle
     private let kLayoutMargins = UIEdgeInsets(top: 12, left: 16, bottom: 16, right: 16)
     private let kImageHeight: CGFloat = 200.0
+
     init(with style: ChoiceCardStyle) {
         viewStyle = style
         super.init(with: style)
     }
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
     override func appendContent(_ content: ChatMessageContent, animated: Bool) {
         switch content {
         case .choiceCard(let choiceCard):
@@ -21,20 +26,22 @@ final class ChoiceCardView: OperatorChatMessageView {
             break
         }
     }
-    private func contentView(for choiceCard: ChoiceCard) -> UIView {
-        let view = UIView()
 
-        view.layer.cornerRadius = 8
-        view.layer.borderWidth = 1
-        view.layer.borderColor = viewStyle.frameColor.cgColor
+    private func contentView(for choiceCard: ChoiceCard) -> UIView {
+        let containerView = UIView()
+
+        containerView.layer.cornerRadius = 8
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = viewStyle.frameColor.cgColor
+
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 12
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = kLayoutMargins
-
-        view.addSubview(stackView)
+        containerView.addSubview(stackView)
         stackView.autoPinEdgesToSuperviewEdges()
+
         if let imageUrl = choiceCard.imageUrl {
             let imageView = ImageView()
             imageView.contentMode = .scaleAspectFill
@@ -44,6 +51,7 @@ final class ChoiceCardView: OperatorChatMessageView {
             imageView.setImage(from: imageUrl, animated: true)
             stackView.addArrangedSubview(imageView)
         }
+
         let textView = ChatTextContentView(
             with: style.text,
             contentAlignment: .left,
@@ -51,11 +59,12 @@ final class ChoiceCardView: OperatorChatMessageView {
         )
         textView.text = choiceCard.text
         stackView.addArrangedSubview(textView)
-        guard let options = choiceCard.options else {
-            return view
-        }
+
+        guard let options = choiceCard.options else { return containerView }
+
         let optionViews: [UIView] = options.compactMap { option in
             let optionView = ChoiceCardOptionView(with: viewStyle.choiceOption, text: option.text)
+
             if let selectedValue = choiceCard.selectedOption {
                 optionView.state = option.value == selectedValue
                     ? .selected
@@ -65,10 +74,11 @@ final class ChoiceCardView: OperatorChatMessageView {
             } else {
                 optionView.state = .disabled
             }
+
             return optionView
         }
-
         stackView.addArrangedSubviews(optionViews)
-        return view
+
+        return containerView
     }
 }
