e9434a3ba6acfda0122cf916a07e4ef16d6c95a1
KarimovaKarina <karimovakarina180@gmail.com>
2021-09-28 16:03:36 +0300

    [MUIC-375], [MUIC-285], [MUIC-390], [MUIC-287]

--- GliaWidgets/View/Chat/Message/Content/Choice/ChoiceCardOptionStyle.swift
+++ GliaWidgets/View/Chat/Message/Content/Choice/ChoiceCardOptionStyle.swift
@@ -3,46 +3,27 @@ import UIKit
 /// Styles of the choice card's answer options.
 public final class ChoiceCardOptionStyle {
     /// Style of an option in an active state - choice card has not been answered by the visitor yet.
-    public var normal: ChatTextContentStyle
+    public var normal: ChoiceCardOptionStateStyle
 
     /// Style of an option in a selected state - choice card has already been answered by the visitor.
-    public var selected: ChatTextContentStyle
+    public var selected: ChoiceCardOptionStateStyle
 
     /// Style of an option in a disabled state - choice card has already been answered by the visitor or the choice card became inactive (e.g. engagement ended).
-    public var disabled: ChatTextContentStyle
+    public var disabled: ChoiceCardOptionStateStyle
 
     ///
     /// - Parameters:
-    ///   - textFont: Font of an option's text.
-    ///   - normalTextColor: Color of an active option's text.
-    ///   - normalBackgroundColor: Color of an active option's background.
-    ///   - highlightedTextColor: Color of a selected option's text.
-    ///   - highlightedBackgroundColor: Color of a selected option's background.
-    ///   - disabledTextColor: Color of a disabled option's text.
-    ///   - disabledBackgroundColor: Color of a disabled option's background.
+    ///   - normal: Style of an option in an active state - choice card has not been answered by the visitor yet.
+    ///   - selected: Style of an option in a selected state - choice card has already been answered by the visitor.
+    ///   - disabled: Style of an option in a disabled state - choice card has already been answered by the visitor or the choice card became inactive (e.g. engagement ended).
+    ///
     public init(
-        textFont: UIFont,
-        normalTextColor: UIColor,
-        normalBackgroundColor: UIColor,
-        highlightedTextColor: UIColor,
-        highlightedBackgroundColor: UIColor,
-        disabledTextColor: UIColor,
-        disabledBackgroundColor: UIColor
+        normal: ChoiceCardOptionStateStyle,
+        selected: ChoiceCardOptionStateStyle,
+        disabled: ChoiceCardOptionStateStyle
     ) {
-        self.normal = ChatTextContentStyle(
-            textFont: textFont,
-            textColor: normalTextColor,
-            backgroundColor: normalBackgroundColor
-        )
-        self.selected = ChatTextContentStyle(
-            textFont: textFont,
-            textColor: highlightedTextColor,
-            backgroundColor: highlightedBackgroundColor
-        )
-        self.disabled = ChatTextContentStyle(
-            textFont: textFont,
-            textColor: disabledTextColor,
-            backgroundColor: disabledBackgroundColor
-        )
+        self.normal = normal
+        self.selected = selected
+        self.disabled = disabled
     }
 }
