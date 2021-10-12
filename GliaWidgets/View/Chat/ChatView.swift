3e69b47c90414a747474c61f63326013e6f10835
KarimovaKarina <karimovakarina180@gmail.com>
2021-10-12 14:09:32 +0300

    [MUIC-488] Clickable url link

--- GliaWidgets/View/Chat/ChatView.swift
+++ GliaWidgets/View/Chat/ChatView.swift
@@ -14,6 +14,7 @@ class ChatView: EngagementView {
     var callBubbleTapped: (() -> Void)?
     var choiceOptionSelected: ((ChatChoiceCardOption, String) -> Void)!
     var chatScrolledToBottom: ((Bool) -> Void)?
+    var linkTapped: ((URL) -> Void)?
 
     private let style: ChatStyle
     private var messageEntryViewBottomConstraint: NSLayoutConstraint!
@@ -227,12 +228,14 @@ class ChatView: EngagementView {
             view.appendContent(.text(message.content), animated: false)
             view.appendContent(.files(message.files), animated: false)
             view.fileTapped = { [weak self] in self?.fileTapped?($0) }
+            view.linkTapped = { [weak self] in self?.linkTapped?($0) }
             return .outgoingMessage(view)
         case .visitorMessage(let message, let status):
             let view = VisitorChatMessageView(with: style.visitorMessage)
             view.appendContent(.text(message.content), animated: false)
             view.appendContent(.downloads(message.downloads), animated: false)
             view.downloadTapped = { [weak self] in self?.downloadTapped?($0) }
+            view.linkTapped = { [weak self] in self?.linkTapped?($0) }
             view.status = status
             return .visitorMessage(view)
         case .operatorMessage(let message, let showsImage, let imageUrl):
@@ -240,6 +243,7 @@ class ChatView: EngagementView {
             view.appendContent(.text(message.content), animated: false)
             view.appendContent(.downloads(message.downloads), animated: false)
             view.downloadTapped = { [weak self] in self?.downloadTapped?($0) }
+            view.linkTapped = { [weak self] in self?.linkTapped?($0) }
             view.showsOperatorImage = showsImage
             view.setOperatorImage(fromUrl: imageUrl, animated: false)
             return .operatorMessage(view)
