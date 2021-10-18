b67ccab03721d247be8d9c483d3cceb334e9a1fc
Karina Karimova <karimovakarina180@gmail.com>
2021-10-18 14:48:39 +0300

    [MUIC-468] Fix video bar buttons in portrait mode

--- GliaWidgets/View/Call/CallView.swift
+++ GliaWidgets/View/Call/CallView.swift
@@ -32,7 +32,6 @@ class CallView: EngagementView {
     private let kRemoteVideoViewPortraitHeightMultiplier: CGFloat = 0.3
     private let kRemoteVideoViewLandscapeHeightMultiplier: CGFloat = 1.0
     private let kBarsHideDelay: TimeInterval = 3.2
-    private var barsAreHidden: Bool = false
 
     init(with style: CallStyle) {
         self.style = style
@@ -91,14 +90,8 @@ class CallView: EngagementView {
 
     func checkBarsOrientation() {
         guard mode == .video else { return }
-        if barsAreHidden {
-            let newHeaderConstraint = -header.frame.size.height + safeAreaInsets.top
-            headerTopConstraint.constant = newHeaderConstraint
-            buttonBarBottomConstraint.constant = buttonBar.frame.size.height
-        } else {
-            headerTopConstraint.constant = 0
-            buttonBarBottomConstraint.constant = 0
-        }
+        headerTopConstraint.constant = 0
+        buttonBarBottomConstraint.constant = 0
     }
 
     private func setup() {
@@ -244,7 +237,6 @@ class CallView: EngagementView {
             self.buttonBarBottomConstraint.constant = 0
             self.layoutIfNeeded()
         }
-        barsAreHidden = true
     }
 
     private func hideBars(duration: TimeInterval) {
@@ -255,7 +247,6 @@ class CallView: EngagementView {
             self.buttonBarBottomConstraint.constant = self.buttonBar.frame.size.height
             self.layoutIfNeeded()
         }
-        barsAreHidden = false
     }
 
     private func hideLandscapeBars() {
