c2f92bb86d0147422f4280d4003eb0b652697890
Kuldar-Daniel Kokorev <kuldar.daniel@gmail.com>
2022-01-20 14:21:18 +0200

    The video is not correctly shown in landscape mode (#218) (#220)
    * The video is not correctly shown in landscape mode

--- GliaWidgets/View/Call/Video/VideoStreamView.swift
+++ GliaWidgets/View/Call/Video/VideoStreamView.swift
@@ -1,11 +1,14 @@
 import UIKit
 import SalemoveSDK
 
-class VideoStreamView: View {
+class VideoStreamView: UIView {
     enum Kind {
         case local
         case remote
     }
+
+    var pan: ((CGPoint) -> Void)?
+    var show: ((Bool) -> Void)?
 
     weak var streamView: StreamView? {
         didSet {
@@ -17,13 +20,23 @@ class VideoStreamView: View {
 
     init(_ kind: Kind) {
         self.kind = kind
-        super.init()
+
+        super.init(frame: .zero)
+
         setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     private func setup() {
         clipsToBounds = true
         layer.cornerRadius = kind == .local ? 6.0 : 0.0
+
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan(_:)))
+        addGestureRecognizer(panRecognizer)
     }
 
     private func replace(
@@ -31,10 +44,23 @@ class VideoStreamView: View {
         with streamView: StreamView?
     ) {
         oldStreamView?.removeFromSuperview()
-        guard let streamView = streamView else { return }
-        streamView.scale = .aspectFill
+        guard let streamView = streamView else {
+            show?(false)
+            return
+        }
+        streamView.scale = kind == .remote ? .aspectFit : .aspectFill
         addSubview(streamView)
         streamView.autoPinEdgesToSuperviewEdges()
         streamView.layoutIfNeeded()
+        show?(true)
+    }
+
+    @objc
+    private func pan(_ gesture: UIPanGestureRecognizer) {
+        guard gesture.view != nil else { return }
+
+        let translation = gesture.translation(in: self)
+        pan?(translation)
+        gesture.setTranslation(.zero, in: self)
     }
 }
