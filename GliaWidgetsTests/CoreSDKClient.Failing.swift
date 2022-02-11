e9e556b2c910f40250b05ef5165b7e8e8ee75c6b
Igor Kravchenko <igman2005@gmail.com>
2022-02-11 16:46:32 +0200

    fixup! Introduce failable mocks
    Update failable mocks to provide more context

--- GliaWidgetsTests/CoreSDKClient.Failing.swift
+++ GliaWidgetsTests/CoreSDKClient.Failing.swift
@@ -1,32 +1,31 @@
 @testable import GliaWidgets
-import XCTest
 
 extension CoreSdkClient {
     static let failing = Self(
         pushNotifications: .failing,
         createAppDelegate: { .failing },
-        clearSession: { XCTFail("clearSession") },
-        fetchVisitorInfo: { _ in XCTFail("fetchVisitorInfo") },
-        updateVisitorInfo: { _, _ in XCTFail("updateVisitorInfo") },
-        sendSelectedOptionValue: { _, _ in XCTFail("sendSelectedOptionValue") },
-        configureWithConfiguration: { _, _ in XCTFail("configureWithConfiguration") },
-        configureWithInteractor: { _ in XCTFail("configureWithInteractor") },
-        queueForEngagement: { _, _, _, _, _, _ in XCTFail("queueForEngagement") },
-        requestMediaUpgradeWithOffer: { _, _ in XCTFail("requestMediaUpgradeWithOffer") },
-        sendMessagePreview: { _, _ in XCTFail("sendMessagePreview") },
-        sendMessageWithAttachment: { _, _, _ in XCTFail("sendMessageWithAttachment") },
-        cancelQueueTicket: { _, _ in XCTFail("cancelQueueTicket") },
-        endEngagement: { _ in XCTFail("endEngagement") },
-        requestEngagedOperator: { _ in XCTFail("requestEngagedOperator") },
-        uploadFileToEngagement: { _, _, _ in XCTFail("uploadFileToEngagement") },
-        fetchFile: { _, _, _ in XCTFail("fetchFile") }
+        clearSession: { fail("\(Self.self).clearSession") },
+        fetchVisitorInfo: { _ in fail("\(Self.self).fetchVisitorInfo") },
+        updateVisitorInfo: { _, _ in fail("\(Self.self).updateVisitorInfo") },
+        sendSelectedOptionValue: { _, _ in fail("\(Self.self).sendSelectedOptionValue") },
+        configureWithConfiguration: { _, _ in fail("\(Self.self).configureWithConfiguration") },
+        configureWithInteractor: { _ in fail("\(Self.self).configureWithInteractor") },
+        queueForEngagement: { _, _, _, _, _, _ in fail("\(Self.self).queueForEngagement") },
+        requestMediaUpgradeWithOffer: { _, _ in fail("\(Self.self).requestMediaUpgradeWithOffer") },
+        sendMessagePreview: { _, _ in fail("\(Self.self).sendMessagePreview") },
+        sendMessageWithAttachment: { _, _, _ in fail("\(Self.self).sendMessageWithAttachment") },
+        cancelQueueTicket: { _, _ in fail("cancelQueueTicket") },
+        endEngagement: { _ in fail("\(Self.self).endEngagement") },
+        requestEngagedOperator: { _ in fail("\(Self.self).requestEngagedOperator") },
+        uploadFileToEngagement: { _, _, _ in fail("\(Self.self).uploadFileToEngagement") },
+        fetchFile: { _, _, _ in fail("\(Self.self).fetchFile") }
     )
 }
 
 extension CoreSdkClient.PushNotifications {
     static let failing = Self(
         applicationDidRegisterForRemoteNotificationsWithDeviceToken: { _, _ in
-            XCTFail("applicationDidRegisterForRemoteNotificationsWithDeviceToken")
+            fail("\(Self.self).applicationDidRegisterForRemoteNotificationsWithDeviceToken")
         }
     )
 }
@@ -34,11 +33,11 @@ extension CoreSdkClient.PushNotifications {
 extension CoreSdkClient.AppDelegate {
     static let failing = Self(
         applicationDidFinishLaunchingWithOptions: { _, _ in
-            XCTFail("applicationDidFinishLaunchingWithOptions")
+            fail("\(Self.self).applicationDidFinishLaunchingWithOptions")
             return false
         },
         applicationDidBecomeActive: { _ in
-            XCTFail("applicationDidBecomeActive")
+            fail("\(Self.self).applicationDidBecomeActive")
         }
     )
 }
