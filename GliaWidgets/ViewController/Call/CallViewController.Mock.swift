fed2ae227ace4eecec0120d64f26e75473a3f32f
Igor Kravchenko <igman2005@gmail.com>
2022-03-21 21:14:37 +0200

    fixup! Add snapshot tests for Voice Over
    Take Visitor video into account for VoiceOver

--- GliaWidgets/ViewController/Call/CallViewController.Mock.swift
+++ GliaWidgets/ViewController/Call/CallViewController.Mock.swift
@@ -164,44 +164,6 @@ extension CallViewController {
         return viewController
     }
 
-    static func mockVideoCallConnectedState() throws -> CallViewController {
-        let conf = try CoreSdkClient.Salemove.Configuration.mock()
-        let queueId = UUID.mock.uuidString
-        let viewContext = CoreSdkClient.VisitorContext.mock
-        let interactorEnv = Interactor.Environment.mock
-        let interactor = Interactor.mock(
-            configuration: conf,
-            queueID: queueId,
-            visitorContext: viewContext,
-            environment: interactorEnv
-        )
-        let alertConf = AlertConfiguration.mock()
-        let screenShareHandler = ScreenShareHandler()
-        let callKind = CallKind.video(direction: .oneWay)
-        let callEnv = Call.Environment.mock
-        let call = Call.mock(kind: callKind, environment: callEnv)
-        let unreadMessages = ObservableValue<Int>.init(with: .zero)
-        let startAction = CallViewModel.StartAction.engagement(mediaType: .audio)
-        let viewModel = CallViewModel.mock(
-            interactor: interactor,
-            alertConfiguration: alertConf,
-            screenShareHandler: screenShareHandler,
-            call: call,
-            unreadMessages: unreadMessages,
-            startWith: startAction
-        )
-        let theme = Theme.mock()
-        let viewFactEnv = ViewFactory.Environment.mock
-        let viewFactory: ViewFactory = .mock(
-            theme: theme,
-            environment: viewFactEnv
-        )
-        let viewController = CallViewController.mock(viewModel: viewModel, viewFactory: viewFactory)
-        viewController.view.frame = UIScreen.main.bounds
-        viewModel.action?(.connected(name: "Blobby Blob", imageUrl: nil))
-        return viewController
-    }
-
     static func mockVideoCallQueueState() throws -> CallViewController {
         let conf = try CoreSdkClient.Salemove.Configuration.mock()
         let queueId = UUID.mock.uuidString
@@ -237,6 +199,53 @@ extension CallViewController {
         let viewController = CallViewController.mock(viewModel: viewModel, viewFactory: viewFactory)
         viewController.view.frame = UIScreen.main.bounds
         viewModel.action?(.queue)
+        return viewController
+    }
+
+    static func mockVideoCallConnectedState() throws -> CallViewController {
+        let conf = try CoreSdkClient.Salemove.Configuration.mock()
+        let queueId = UUID.mock.uuidString
+        let viewContext = CoreSdkClient.VisitorContext.mock
+        let interactorEnv = Interactor.Environment.mock
+        let interactor = Interactor.mock(
+            configuration: conf,
+            queueID: queueId,
+            visitorContext: viewContext,
+            environment: interactorEnv
+        )
+        let alertConf = AlertConfiguration.mock()
+        let screenShareHandler = ScreenShareHandler()
+        let callKind = CallKind.video(direction: .oneWay)
+        let callEnv = Call.Environment.mock
+        let call = Call.mock(kind: callKind, environment: callEnv)
+        let unreadMessages = ObservableValue<Int>.init(with: .zero)
+        let startAction = CallViewModel.StartAction.engagement(mediaType: .video)
+        let viewModel = CallViewModel.mock(
+            interactor: interactor,
+            alertConfiguration: alertConf,
+            screenShareHandler: screenShareHandler,
+            call: call,
+            unreadMessages: unreadMessages,
+            startWith: startAction
+        )
+        let theme = Theme.mock()
+        let viewFactEnv = ViewFactory.Environment.mock
+        let viewFactory: ViewFactory = .mock(
+            theme: theme,
+            environment: viewFactEnv
+        )
+        let viewController = CallViewController.mock(viewModel: viewModel, viewFactory: viewFactory)
+        viewController.view.frame = UIScreen.main.bounds
+        viewModel.action?(.connected(name: "Blobby Blob", imageUrl: nil))
+        viewModel.action?(.switchToVideoMode)
+        viewModel.action?(.setButtonEnabled(.video, enabled: true))
+        viewModel.action?(.setButtonEnabled(.chat, enabled: true))
+        viewModel.action?(.setButtonEnabled(.mute, enabled: true))
+        viewModel.action?(.setButtonEnabled(.speaker, enabled: true))
+
+
+
+        call.updateVideoStream(with: CoreSdkClient.MockVideoStreamable.mock())
         return viewController
     }
 }
