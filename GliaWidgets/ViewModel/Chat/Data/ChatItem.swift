6fdf411c6f73d4ff700da3b8d2ed90f79f4ddd26
Kuldar-Daniel Kokorev <kuldar-daniel.kokorev@mooncascade.com>
2022-05-09 11:31:22 +0300

    Move chatitem equatable to test target

--- GliaWidgets/ViewModel/Chat/Data/ChatItem.swift
+++ GliaWidgets/ViewModel/Chat/Data/ChatItem.swift
@@ -45,34 +45,3 @@ extension ChatItem {
         case operatorConnected(name: String?, imageUrl: String?)
     }
 }
-
-extension ChatItem.Kind: Equatable {
-    static func == (lhs: ChatItem.Kind, rhs: ChatItem.Kind) -> Bool {
-        switch (lhs, rhs) {
-        case (.queueOperator, .queueOperator):
-            return true
-
-        case (.outgoingMessage(let lhsMessage), .outgoingMessage(let rhsMessage)):
-            return lhsMessage.id == rhsMessage.id
-
-        case (.visitorMessage(let lhsMessage, _), .visitorMessage(let rhsMessage, _)):
-            return lhsMessage.id == rhsMessage.id
-
-        case (.operatorMessage(let lhsMessage, _, _), .operatorMessage(let rhsMessage, _, _)):
-            return lhsMessage.id == rhsMessage.id
-
-        case (.choiceCard(let lhsMessage, _, _, _), .choiceCard(let rhsMessage, _, _, _)):
-            return lhsMessage.id == rhsMessage.id
-
-        case (.callUpgrade(let lhsKind, let lhsDuration), .callUpgrade(let rhsKind, let rhsDuration)):
-            return lhsKind.value == rhsKind.value
-                && lhsDuration.value == rhsDuration.value
-
-        case (.operatorConnected(let lhsName, let lhsImageUrl), .operatorConnected(let rhsName, let rhsImageUrl)):
-            return lhsName == rhsName
-                && lhsImageUrl == rhsImageUrl
-        default:
-            return false
-        }
-    }
-}
