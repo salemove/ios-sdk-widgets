166316db15fc752f9ffd603eca01ffa603cf8207
Yurii Dukhovnyi <y.dukhovnyi@gmail.com>
2022-05-13 17:40:10 +0300

    fixup! Rebase fix

--- GliaWidgetsTests/Sources/ChatViewModelTests.swift
+++ GliaWidgetsTests/Sources/ChatViewModelTests.swift
@@ -178,7 +178,7 @@ class ChatViewModelTests: XCTestCase {
         // Given
         enum Calls { case fetchSiteConfigurations }
         var calls: [Calls] = []
-        let interactorEnv = Interactor.Environment.init(coreSdk: .failing)
+        let interactorEnv = Interactor.Environment(coreSdk: .failing, gcd: .mock)
         let interactor = Interactor.mock(environment: interactorEnv)
         var viewModelEnv = ChatViewModel.Environment.failing
         viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
@@ -199,7 +199,7 @@ class ChatViewModelTests: XCTestCase {
         // Given
         enum Calls { case fetchSiteConfigurations }
         var calls: [Calls] = []
-        let interactorEnv = Interactor.Environment.init(coreSdk: .failing)
+        let interactorEnv = Interactor.Environment.init(coreSdk: .failing, gcd: .mock)
         let interactor = Interactor.mock(environment: interactorEnv)
         var viewModelEnv = ChatViewModel.Environment.failing
         viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
@@ -215,171 +215,6 @@ class ChatViewModelTests: XCTestCase {
         // Then
         XCTAssertEqual(calls, [.fetchSiteConfigurations])
 	}
-
-    func test_onInteractorStateEngagedClearsChatQueueSection() throws {
-        var viewModelEnv = ChatViewModel.Environment.failing
-        viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
-        viewModelEnv.fileManager.createDirectoryAtUrlWithIntermediateDirectories = { _, _, _ in }
-
-        let interactor: Interactor = try .mock()
-        let viewModel: ChatViewModel = .mock(interactor: interactor, environment: viewModelEnv)
-        let queueSectionIndex: Int = viewModel.queueOperatorSection.index
-        let mockOperator: CoreSdkClient.Operator = .mock()
-
-        XCTAssertEqual(0, viewModel.numberOfItems(in: queueSectionIndex))
-        viewModel.update(for: .enqueueing)
-        XCTAssertEqual(1, viewModel.numberOfItems(in: queueSectionIndex))
-        viewModel.update(for: .engaged(mockOperator))
-        XCTAssertEqual(0, viewModel.numberOfItems(in: queueSectionIndex))
-    }
-    
-    func test_onEngagementTransferAddsOperatorConnectedChatItemToTheEndOfChat() throws {
-        var interactorEnv: Interactor.Environment = .failing
-        interactorEnv.gcd.mainQueue.asyncIfNeeded = { $0() }
-        var viewModelEnv = ChatViewModel.Environment.failing
-        viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
-        viewModelEnv.fileManager.createDirectoryAtUrlWithIntermediateDirectories = { _, _, _ in }
-        viewModelEnv.chatStorage.messages = { _ in [] }
-        viewModelEnv.fromHistory = { true }
-
-        let interactor: Interactor = .mock(environment: interactorEnv)
-        let viewModel: ChatViewModel = .mock(interactor: interactor, environment: viewModelEnv)
-        let mockOperator: CoreSdkClient.Operator = .mock()
-        let mockItemKind: ChatItem.Kind = .operatorConnected(name: mockOperator.firstName, imageUrl: mockOperator.picture?.url)
-
-        interactor.onEngagementTransfer([mockOperator])
-
-        let lastSectionIndex = viewModel.numberOfSections - 1
-        let lastSectionLastItemIndex = viewModel.numberOfItems(in: lastSectionIndex) - 1
-        let lastItemKind = viewModel.item(for: lastSectionLastItemIndex, in: lastSectionIndex).kind
-
-        XCTAssertEqual(lastItemKind, mockItemKind)
-    }
-
-    func test__sendMessageCallsEnqueueNewEngagementOnNoneState() throws {
-        // Given
-        enum Call {
-            case configureWithConfiguration, configureWithInteractor, queueForEngagement, sendMessage
-        }
-        var calls: [Call] = []
-        var coreSdk = CoreSdkClient.failing
-        coreSdk.sendMessagePreview = { _, _ in }
-        coreSdk.configureWithConfiguration = { _, completion in
-            calls.append(.configureWithConfiguration)
-            completion?()
-        }
-        coreSdk.configureWithInteractor = { _ in
-            calls.append(.configureWithInteractor)
-        }
-        coreSdk.queueForEngagement = { _, _, _, _, _, _ in
-            calls.append(.queueForEngagement)
-        }
-        coreSdk.sendMessageWithAttachment = { _, _, _ in
-            calls.append(.sendMessage)
-        }
-        let interactor = Interactor.mock(environment: .init(coreSdk: coreSdk))
-
-        var viewModelEnv = ChatViewModel.Environment.failing
-        viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
-        viewModelEnv.fileManager.createDirectoryAtUrlWithIntermediateDirectories = { _, _, _ in }
-        viewModelEnv.chatStorage.messages = { _ in [] }
-        let viewModel = ChatViewModel.mock(interactor: interactor, environment: viewModelEnv)
-
-        // When
-        // Sets message text
-        viewModel.event(.messageTextChanged("Message"))
-        // Puts outgoing message to pending messages and starts enqueueing engagement
-        viewModel.event(.sendTapped)
-
-        // Then
-        XCTAssertEqual(calls, [
-            .configureWithInteractor,
-            .configureWithConfiguration,
-            .queueForEngagement
-        ])
-    }
-
-    func _test__sendMessageDoesNotCallEnqueueNewEngagementOnEnqueuedState() throws {
-        // Given
-        enum Call { case queueForEngagement, sendMessage }
-        var calls: [Call] = []
-        var coreSdk = CoreSdkClient.failing
-        coreSdk.sendMessagePreview = { _, _ in }
-        coreSdk.configureWithConfiguration = { _, completion in
-            completion?()
-        }
-        coreSdk.configureWithInteractor = { _ in }
-        coreSdk.queueForEngagement = { _, _, _, _, _, _ in
-            #warning("Need to call competion after adding public init for QueueTicket core-sdk entity")
-            calls.append(.queueForEngagement)
-        }
-        coreSdk.sendMessageWithAttachment = { _, _, _ in
-            calls.append(.sendMessage)
-        }
-        let interactor = Interactor.mock(environment: .init(coreSdk: coreSdk))
-
-        var viewModelEnv = ChatViewModel.Environment.failing
-        viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
-        viewModelEnv.fileManager.createDirectoryAtUrlWithIntermediateDirectories = { _, _, _ in }
-        viewModelEnv.chatStorage.messages = { _ in [] }
-        viewModelEnv.chatStorage.isEmpty = { true }
-        let viewModel = ChatViewModel.mock(interactor: interactor, environment: viewModelEnv)
-
-        // When
-        // Sets interactor state to enqueued
-        viewModel.start()
-        // Sets message text
-        viewModel.event(.messageTextChanged("Message"))
-        // Sends outgoing message
-        viewModel.event(.sendTapped)
-
-        // Then
-        XCTAssertEqual(calls, [.queueForEngagement])
-    }
-
-    func _test__sendMessageDoesNotCallEnqueueNewEngagementOnEngagedState() throws {
-        // Given
-        enum Call { case queueForEngagement, sendMessage }
-        var calls: [Call] = []
-        var coreSdk = CoreSdkClient.failing
-        coreSdk.sendMessagePreview = { _, _ in }
-        coreSdk.configureWithConfiguration = { _, completion in
-            completion?()
-        }
-        coreSdk.configureWithInteractor = { _ in }
-        coreSdk.queueForEngagement = { _, _, _, _, _, _ in
-            #warning("Need to call competion after adding public init for QueueTicket core-sdk entity")
-            calls.append(.queueForEngagement)
-        }
-        coreSdk.requestEngagedOperator = { completion in
-            completion([.mock()], nil)
-        }
-        coreSdk.sendMessageWithAttachment = { _, _, _ in
-            calls.append(.sendMessage)
-        }
-        let interactor = Interactor.mock(environment: .init(coreSdk: coreSdk))
-
-        var viewModelEnv = ChatViewModel.Environment.failing
-        viewModelEnv.fileManager.urlsForDirectoryInDomainMask = { _, _ in [.mock] }
-        viewModelEnv.fileManager.createDirectoryAtUrlWithIntermediateDirectories = { _, _, _ in }
-        viewModelEnv.chatStorage.messages = { _ in [] }
-        viewModelEnv.chatStorage.isEmpty = { true }
-        let viewModel = ChatViewModel.mock(interactor: interactor, environment: viewModelEnv)
-
-        // When
-
-        // Sets interactor state to enqueued
-        viewModel.start()
-        // Sets interactor state to engaged
-        interactor.start()
-        // Sets message text
-        viewModel.event(.messageTextChanged("Message"))
-        // Sends outgoing message
-        viewModel.event(.sendTapped)
-
-        // Then
-        XCTAssertEqual(calls, [.queueForEngagement, .sendMessage])
-    }
 }
 
 extension ChatChoiceCardOption {
