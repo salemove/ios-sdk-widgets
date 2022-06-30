82281ef14f35c7eb0831bfd3dda29269d1cb2b5b
Kuldar-Daniel Kokorev <kuldar-daniel.kokorev@mooncascade.com>
2022-06-30 10:59:57 +0300

    Mark required initialisers unavailable

--- GliaWidgets/View/Chat/Message/Content/Text/ChatTextContentView.swift
+++ GliaWidgets/View/Chat/Message/Content/Text/ChatTextContentView.swift
@@ -47,6 +47,7 @@ class ChatTextContentView: UIView {
         layout()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
