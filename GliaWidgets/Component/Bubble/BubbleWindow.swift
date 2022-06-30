be9caf3830a8aa8eebeaf8a9df5246cc6b183ee2
Kuldar-Daniel Kokorev <kuldar-daniel.kokorev@mooncascade.com>
2022-06-30 10:59:57 +0300

    Mark required initialisers unavailable

--- GliaWidgets/Component/Bubble/BubbleWindow.swift
+++ GliaWidgets/Component/Bubble/BubbleWindow.swift
@@ -34,6 +34,7 @@ class BubbleWindow: UIWindow {
         layout()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- GliaWidgets/View/Chat/Message/VisitorChatMessageView.swift
+++ GliaWidgets/View/Chat/Message/VisitorChatMessageView.swift
@@ -21,6 +21,7 @@ class VisitorChatMessageView: ChatMessageView {
         layout()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
