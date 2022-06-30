b74757ede1e6bf30e516698d490fb62347f50ae9
Kuldar-Daniel Kokorev <kuldar-daniel.kokorev@mooncascade.com>
2022-06-30 10:59:57 +0300

    Mark required initialisers unavailable

--- GliaWidgets/View/Chat/Message/Content/File/Image/ChatImageFileContentView.swift
+++ GliaWidgets/View/Chat/Message/Content/File/Image/ChatImageFileContentView.swift
@@ -25,6 +25,7 @@ class ChatImageFileContentView: ChatFileContentView {
         layout()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- GliaWidgets/View/Chat/Upgrade/ChatCallUpgradeView.swift
+++ GliaWidgets/View/Chat/Upgrade/ChatCallUpgradeView.swift
@@ -25,6 +25,7 @@ class ChatCallUpgradeView: UIView {
         duration.removeObserver(self)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
