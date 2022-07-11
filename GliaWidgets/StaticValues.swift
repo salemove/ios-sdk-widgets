96ac88b81b568c86ba64da2b4124f61b166dcf79
Gerson Noboa <gerson.noboa@glia.com>
2022-07-11 13:16:07 +0300

    Add support for incrementing project version through fastlane
    To make it easier to upgrade versions of the widgets, `fastlane` is
    added. With it, a lane is created to update the version in the
    Info.plists. Also, an action is added that goes into the
    `StaticValues.swift` file and modifies the value of it. This is to
    avoid having logs with incorrect versions in case integrators modify
    our Info.plist files somehow. This action is then used in the
    `increment_project_version` lane as well.
    
    MOB-1375

---
+++ GliaWidgets/StaticValues.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+final class StaticValues {
+    /// Current SDK version. This version gets synced with the Info.plist version
+    /// when incrementing the version through Fastlane. Unlike the Info.plist, this
+    /// version cannot be changed by integrators, so this ensures that our code will
+    /// always have the correct version regardless of what our integrators do with
+    /// our plist files.
+    static let sdkVersion = "0.9.0"
+}
