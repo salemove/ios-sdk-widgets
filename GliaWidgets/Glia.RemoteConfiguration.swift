f1777bcbbb090670960c541ebc2a2e26f6c482a6
Yurii Dukhovnyi <y.dukhovnyi@gmail.com>
2022-09-09 23:33:09 +0300

    Introduce base structures for unified customization

---
+++ GliaWidgets/Glia.RemoteConfiguration.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+extension Glia {
+
+    public func startEngagementWithConfig(
+        engagement: EngagementKind,
+        uiConfig: RemoteConfiguration,
+        theme: Theme = Theme(),
+        features: Features = .all,
+        sceneProvider: SceneProvider? = nil
+    ) throws {
+
+        // Apply remote configuration
+
+        try startEngagement(
+            engagementKind: engagement,
+            theme: theme,
+            features: features,
+            sceneProvider: sceneProvider
+        )
+    }
+}
