efa8969eecc1f455f2a7c109414683eb9cfbb9d4
Egor Egorov <egorov.egor@glia.com>
2022-10-05 16:11:56 +0300

    Removed theme from startEngagementWithConfig method params

--- GliaWidgets/Glia.RemoteConfiguration.swift
+++ GliaWidgets/Glia.RemoteConfiguration.swift
@@ -2,15 +2,33 @@ import Foundation
 
 extension Glia {
 
+    /// Starts the engagement.
+    ///
+    /// - Parameters:
+    ///   - engagementKind: Engagement media type.
+    ///   - uiConfig: Remote UI configuration.
+    ///   - features: Set of features to be enabled in the SDK.
+    ///   - sceneProvider: Used to provide `UIWindowScene` to the framework. Defaults to the first active foreground scene.
+    ///
+    /// - throws:
+    ///   - `SalemoveSDK.ConfigurationError.invalidSite`
+    ///   - `SalemoveSDK.ConfigurationError.invalidEnvironment`
+    ///   - `SalemoveSDK.ConfigurationError.invalidAppToken`
+    ///   - `GliaError.engagementExists
+    ///   - `GliaError.sdkIsNotConfigured`
+    ///
+    /// - Important: Note, that `configure(with:queueID:visitorContext:)` must be called initially prior to this method,
+    /// because `GliaError.sdkIsNotConfigured` will occur otherwise.
+    ///
     public func startEngagementWithConfig(
         engagement: EngagementKind,
         uiConfig: RemoteConfiguration,
-        theme: Theme = Theme(),
         features: Features = .all,
         sceneProvider: SceneProvider? = nil
     ) throws {
 
         // Apply remote configuration
+        let theme = Theme()
         theme.applyRemoteConfiguration(uiConfig)
 
         try startEngagement(
