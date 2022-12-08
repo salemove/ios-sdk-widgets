41f4354daab9c92acbd210cdebd38bc5ae537f35
Gerson Noboa <gerson.noboa@glia.com>
2022-12-08 08:52:13 +0200

    Add feature flag for secure conversations
    In order to keep the master branch releasable, a temporary feature
    flag has been added, using the current `Features` class. This has been
    applied to entry points of the secure conversations functionality.
    
    MOB-1697

--- TestingApp/ViewController.swift
+++ TestingApp/ViewController.swift
@@ -22,10 +22,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         title = "Glia UI testing"
         view.backgroundColor = .white
+
+        if !features.contains(.secureConversations) {
+            secureConversationsButton.isHidden = true
+        }
     }
     @IBOutlet weak var visitorCodeView: UIView!
 
     @IBOutlet var toggleAuthenticateButton: UIButton!
+    @IBOutlet var secureConversationsButton: UIButton!
 
     @IBAction private func settingsTapped() {
         presentSettings()
@@ -48,7 +53,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction private func secureConversationTapped() {
-        presentGlia(.messaging)
+        if features.contains(.secureConversations) {
+            presentGlia(.messaging)
+        }
     }
 
     @IBAction private func clearSessionTapped() {
