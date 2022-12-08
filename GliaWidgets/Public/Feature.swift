9664b77dcbb1f0bfa5efacbf52c8caee893108ac
Gerson Noboa <gerson.noboa@glia.com>
2022-12-08 08:52:13 +0200

    Add feature flag for secure conversations
    In order to keep the master branch releasable, a temporary feature
    flag has been added, using the current `Features` class. This has been
    applied to entry points of the secure conversations functionality.
    
    MOB-1697

--- GliaWidgets/Public/Feature.swift
+++ GliaWidgets/Public/Feature.swift
@@ -10,6 +10,7 @@ public struct Features: OptionSet {
 
     /// Bubble that is shown in engagement time outside of engagement view.
     public static let bubbleView = Self(rawValue: 1 << 0)
+    public static let secureConversations = Self(rawValue: 2 << 0)
 
     public static let all: Self = [.bubbleView]
 }
--- TestingApp/ViewController.swift
+++ TestingApp/ViewController.swift
@@ -22,11 +22,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         title = "Glia UI testing"
         view.backgroundColor = .white
+
+        if !features.contains(.secureConversations) {
+            secureConversationsButton.isHidden = true
+        }
     }
 
     @IBOutlet weak var visitorCodeView: UIView!
     @IBOutlet var toggleAuthenticateButton: UIButton!
     @IBOutlet var configureButton: UIButton!
+    @IBOutlet var secureConversationsButton: UIButton!
 
     @IBAction private func settingsTapped() {
         presentSettings()
@@ -49,7 +54,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction private func secureConversationTapped() {
-        presentGlia(.messaging)
+        if features.contains(.secureConversations) {
+            presentGlia(.messaging)
+        }
     }
 
     @IBAction private func clearSessionTapped() {
