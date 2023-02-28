c8f1481b34d0e9d2b6632516e3dca9b39fd83436
Gerson Noboa <gerson.noboa@gmail.com>
2023-02-28 09:04:26 +0200

    Add accessibility and localization for confirmation screen
    Just as in welcome screen, it is better to add accessibility together
    with localization so that we can use localized accessibility labels
    and hints. At some point someone moved the welcome styles to their own
    files, so that is also done here because it makes it easier to
    navigate each specific style.
    
    MOB-1890

---
+++ GliaWidgets/SecureConversations/Confirmation/ConfirmationStyle/ConfirmationStyle+SubtitleStyle.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+extension SecureConversations.ConfirmationStyle {
+    /// Style for subtitle shown in the confirmation area.
+    public struct SubtitleStyle: Equatable {
+        /// Title text value.
+        public var text: String
+        /// Title font.
+        public var font: UIFont
+        /// Title color.
+        public var color: UIColor
+        /// Accessibility related properties.
+        public var accessibility: Accessibility
+
+        /// - Parameters:
+        ///   - text: Title text value.
+        ///   - font: Title font.
+        ///   - color: Title color.
+        public init(
+            text: String,
+            font: UIFont,
+            color: UIColor,
+            accessibility: Accessibility
+        ) {
+            self.text = text
+            self.font = font
+            self.color = color
+            self.accessibility = accessibility
+        }
+
+        /// Accessibility properties for SubtitleStyle.
+        public struct Accessibility: Equatable {
+            /// Flag that provides font dynamic type by setting `adjustsFontForContentSizeCategory` for component that supports it.
+            public var isFontScalingEnabled: Bool
+
+            /// - Parameter isFontScalingEnabled: Flag that provides font dynamic type by setting `adjustsFontForContentSizeCategory` for component that supports it.
+            public init(isFontScalingEnabled: Bool) {
+                self.isFontScalingEnabled = isFontScalingEnabled
+            }
+
+            /// Accessibility is not supported intentionally.
+            public static let unsupported = Self(isFontScalingEnabled: false)
+        }
+    }
+}
