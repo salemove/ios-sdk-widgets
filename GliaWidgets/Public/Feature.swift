73a63eaabf71505f519f05bbf84e0ae032c8b3cf
Igor Kravchenko <igman2005@gmail.com>
2023-04-05 18:20:45 +0300

    Remove 'secureConversations' feature flag
    Remove 'secureConversations' feature flag since it is no longer needed.
    
    MOB-1707

--- GliaWidgets/Public/Feature.swift
+++ GliaWidgets/Public/Feature.swift
@@ -10,7 +10,5 @@ public struct Features: OptionSet {
 
     /// Bubble that is shown in engagement time outside of engagement view.
     public static let bubbleView = Self(rawValue: 1 << 0)
-    #warning("Remove 'secureConversations' flag once implementation is complete.")
-    public static let secureConversations = Self(rawValue: 2 << 0)
-    public static let all: Self = [.bubbleView, .secureConversations]
+    public static let all: Self = [.bubbleView]
 }
