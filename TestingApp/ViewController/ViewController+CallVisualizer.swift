686f2a4755075586fb9f2929acc24188032987b1
Egor Egorov <egorov.egor@glia.com>
2023-05-26 11:51:43 +0300

    Update CallVisualizer interfaces
    To align CallVisualizer interfaces between platforms previous method was separated on two methods

--- TestingApp/ViewController/ViewController+CallVisualizer.swift
+++ TestingApp/ViewController/ViewController+CallVisualizer.swift
@@ -4,19 +4,14 @@ import GliaWidgets
 extension ViewController {
 
     @IBAction private func presentVisitorCodeAsAlertTapped() {
-        Glia.sharedInstance.callVisualizer.showVisitorCodeViewController(
-            by: .alert(self)
-        )
+        Glia.sharedInstance.callVisualizer.showVisitorCodeViewController(from: self)
     }
 
     @IBAction private func embedVisitorCodeViewTapped() {
-        Glia.sharedInstance.callVisualizer.showVisitorCodeViewController(
-            by: .embedded(
-                visitorCodeView,
-                onEngagementAccepted: {
-                    self.visitorCodeView.subviews.forEach { $0.removeFromSuperview() }
-                }
-            )
-        )
+        Glia.sharedInstance.callVisualizer.embedVisitorCodeView(
+            into: visitorCodeView
+        ) {
+            self.visitorCodeView.subviews.forEach { $0.removeFromSuperview() }
+        }
     }
 }
