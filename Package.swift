d787888a57e2d7ffba4c2b4a7ed6a3718ab81f3e
Yurii Dukhovnyi <yurii.dukhovnyi@glia.com>
2023-06-02 14:06:16 +0300

    Update Core SDK version in templates

--- Package.swift
+++ Package.swift
@@ -39,8 +39,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GliaCoreSDK",
-            url: "https://github.com/salemove/ios-bundle/releases/download/1.0.1/GliaCoreSDK.xcframework.zip",
-            checksum: "4d43788335a05d573af40171c99d608aa24aea6b557bc83d8f633eedd776fb53"
+            url: "https://github.com/salemove/ios-bundle/releases/download/1.0.2/GliaCoreSDK.xcframework.zip",
+            checksum: "4d997e74bb975db723b7d93a26f2fb3611f735f809a825aa2e89e5226cc602d4"
         ),
         .binaryTarget(
             name: "PureLayoutXcf",
