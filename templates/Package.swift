07109b06a362f9fe5dcef79aa4c84bf075f8202a
Gerson Noboa <gerson.noboa@gmail.com>
2023-06-05 15:48:48 +0300

    Add possibility to update Core SDK version in widgets
    When ios-bundle executes a post release workflow, Bitrise will update
    the version in the Podfile and the checksum, getting the parameters
    sent from ios-bundle. This will also automatically checkout the
    development branch.
    
    MOB-1792

---
+++ templates/Package.swift
@@ -0,0 +1,79 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "GliaWidgets",
+    defaultLocalization: .init(stringLiteral: "en"),
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "GliaWidgets",
+            targets: ["GliaWidgetsSDK"]
+        ),
+        .library(
+            name: "GliaWidgets-xcframework",
+            targets: ["GliaWidgetsSDK-xcframework"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GliaCoreDependency",
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.35.0/GliaCoreDependency.xcframework.zip",
+            checksum: "8e1746da612dad8a130fd872b3058511121b7c6f29e3b0351ecbbffa96971489"
+        ),
+        .binaryTarget(
+            name: "TwilioVoice",
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.33.0/TwilioVoice.xcframework.zip",
+            checksum: "34288e0876a8840fa51d3813046cf1f40a5939079bea23ace5afe6e752f12f9e"
+        ),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.33.0/WebRTC.xcframework.zip",
+            checksum: "f76e410f608d96989ba0312e099697703a37b4414f8f46bb9e30c3d9b4291a52"
+        ),
+        .binaryTarget(
+            name: "GliaCoreSDK",
+            url: "https://github.com/salemove/ios-bundle/releases/download/${CORE_SDK_VERSION}/GliaCoreSDK.xcframework.zip",
+            checksum: "${CORE_SDK_CHECKSUM}"
+        ),
+        .binaryTarget(
+            name: "GliaWidgetsXcf",
+            url: "https://github.com/salemove/ios-sdk-widgets/releases/download/2.0.1/GliaWidgetsXcf.xcframework.zip",
+            checksum: "26f6e0bc6e32f4223f635d8de8aa4877338aed7a323f2127a05980f347ac1f9a"
+        ),
+        .target(
+            name: "GliaWidgets",
+            path: "GliaWidgets",
+            exclude: [
+                "Cartfile.resolved",
+                "Cartfile",
+                "Info.plist"
+            ],
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .target(
+            name: "GliaWidgetsSDK",
+            dependencies: [
+                "GliaCoreSDK",
+                "GliaCoreDependency",
+                "TwilioVoice",
+                "WebRTC",
+                "GliaWidgets"
+            ]
+        ),
+        .target(
+            name: "GliaWidgetsSDK-xcframework",
+            dependencies: [
+                "GliaCoreSDK",
+                "GliaWidgetsXcf",
+                "GliaCoreDependency",
+                "TwilioVoice",
+                "WebRTC"
+            ]
+        )
+    ]
+)
