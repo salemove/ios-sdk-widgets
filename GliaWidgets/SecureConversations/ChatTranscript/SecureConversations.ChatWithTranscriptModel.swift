bbcecce27815a7e8133c787d662148966a956ab3
Gerson Noboa <gerson.noboa@gmail.com>
2023-07-06 18:22:14 +0300

    Run SwiftLint only on changed files
    SwiftLint was deactivated because it would run on every build.
    This commit makes it so that SwiftLint is only executed on files
    changed according to `git status`. To make this work, current
    SwiftLint issues have been fixed. Also, Xcode will report all
    SwiftLint warnings as errors.
    
    MOB-1973

--- GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.ChatWithTranscriptModel.swift
+++ GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.ChatWithTranscriptModel.swift
@@ -767,39 +767,6 @@ extension SecureConversations {
     typealias ChatWithTranscriptModel = SecureChatModel<ChatViewModel, TranscriptModel>
 }
 
-extension SecureConversations.TranscriptModel {
-    struct Environment {
-        var fetchFile: CoreSdkClient.FetchFile
-        var downloadSecureFile: CoreSdkClient.DownloadSecureFile
-        var fileManager: FoundationBased.FileManager
-        var data: FoundationBased.Data
-        var date: () -> Date
-        var gcd: GCD
-        var uiScreen: UIKitBased.UIScreen
-        var createThumbnailGenerator: () -> QuickLookBased.ThumbnailGenerator
-        var createFileDownload: FileDownloader.CreateFileDownload
-        var loadChatMessagesFromHistory: () -> Bool
-        var fetchChatHistory: CoreSdkClient.FetchChatHistory
-        var uiApplication: UIKitBased.UIApplication
-        var sendSecureMessage: CoreSdkClient.SendSecureMessage
-        var queueIds: [String]
-        var listQueues: CoreSdkClient.ListQueues
-        var alertConfiguration: AlertConfiguration
-        var createFileUploadListModel: SecureConversations.FileUploadListViewModel.Create
-        var uuid: () -> UUID
-        var secureUploadFile: CoreSdkClient.SecureConversationsUploadFile
-        var fileUploadListStyle: FileUploadListStyle
-        var fetchSiteConfigurations: CoreSdkClient.FetchSiteConfigurations
-        var getSecureUnreadMessageCount: CoreSdkClient.GetSecureUnreadMessageCount
-        var messagesWithUnreadCountLoaderScheduler: CoreSdkClient.ReactiveSwift.DateScheduler
-        var secureMarkMessagesAsRead: CoreSdkClient.SecureMarkMessagesAsRead
-        var interactor: Interactor
-        var startSocketObservation: CoreSdkClient.StartSocketObservation
-        var stopSocketObservation: CoreSdkClient.StopSocketObservation
-        var sendSelectedOptionValue: CoreSdkClient.SendSelectedOptionValue
-    }
-}
-
 // MARK: History
 extension SecureConversations.TranscriptModel {
     private func loadHistory(_ completion: @escaping ([ChatMessage]) -> Void) {
