94e011a02401ed846548c1bcc22104a40cb99619
Igor Kravchenko <igman2005@gmail.com>
2023-07-17 09:15:23 +0300

    Adjust layout to match snapshots
    Address layout inconsistencies after PureLayout removal.
    
    MOB-2469

---
+++ SnapshotTests/ChatViewControllerLayoutTests.swift
@@ -0,0 +1,55 @@
+@testable import GliaWidgets
+import SnapshotTesting
+import XCTest
+
+class ChatViewControllerLayoutTests: SnapshotTestCase {
+    func test_messagesFromHistory() {
+        let viewController = ChatViewController.mockHistoryMessagesScreen()
+        viewController.view.frame = UIScreen.main.bounds
+        assertSnapshot(
+            matching: viewController,
+            as: .image,
+            named: nameForDevice()
+        )
+    }
+
+    func test_visitorUploadedFileStates() throws {
+        let viewController = try ChatViewController.mockVisitorFileUploadStates()
+        viewController.view.frame = UIScreen.main.bounds
+        assertSnapshot(
+            matching: viewController,
+            as: .image,
+            named: nameForDevice()
+        )
+    }
+
+    func test_choiceCard() throws {
+        let viewController = try ChatViewController.mockChoiceCard()
+        viewController.view.frame = UIScreen.main.bounds
+        assertSnapshot(
+            matching: viewController,
+            as: .image,
+            named: nameForDevice()
+        )
+    }
+
+    func test_visitorFileDownloadStates() throws {
+        var chatMessages: [ChatMessage] = []
+        let viewController = try ChatViewController.mockVisitorFileDownloadStates { messages in
+            chatMessages = messages
+        }
+        viewController.view.frame = UIScreen.main.bounds
+        viewController.view.setNeedsLayout()
+        viewController.view.layoutIfNeeded()
+        XCTAssertEqual(chatMessages.count, 4)
+        chatMessages[0].downloads[0].state.value = .none
+        chatMessages[1].downloads[0].state.value = .downloading(progress: .init(with: 0.5))
+        chatMessages[2].downloads[0].state.value = .downloaded(.mock())
+        chatMessages[3].downloads[0].state.value = .error(.deleted)
+        assertSnapshot(
+            matching: viewController,
+            as: .image,
+            named: self.nameForDevice()
+        )
+    }
+}
---
+++ SnapshotTests/SecureConversationsConfirmationScreenLayoutTests.swift
@@ -0,0 +1,46 @@
+@testable import GliaWidgets
+import SnapshotTesting
+import XCTest
+
+class SecureConversationsConfirmationScreenLayoutTests: SnapshotTestCase {
+    let theme = Theme.mock()
+
+    func test_confirmationView() {
+        let props = Self.makeConfirmationProps(style: theme.secureConversationsConfirmation)
+        let viewController = SecureConversations.ConfirmationViewController(
+            viewModel: .init(environment: .init(confirmationStyle: theme.defaultSecureConversationsConfirmationStyle)),
+            viewFactory: .mock(theme: theme, messageRenderer: nil, environment: .mock),
+            props: props
+        )
+        viewController.view.frame = UIScreen.main.bounds
+
+        assertSnapshot(
+            matching: viewController.view,
+            as: .image,
+            named: self.nameForDevice()
+        )
+    }
+
+    // MARK: - Helpers
+
+    static func headerProps() -> Header.Props {
+        .mock(
+            title: "Secure Conversations",
+            backButton: .init(style: .mock(image: Asset.back.image)),
+            closeButton: .init(style: .mock(image: Asset.close.image))
+        )
+    }
+
+    static func makeConfirmationProps(
+        headerProps: Header.Props = headerProps(),
+        style: SecureConversations.ConfirmationStyle
+    ) -> SecureConversations.ConfirmationViewController.Props {
+        .init(
+            confirmationViewProps: .init(
+                style: style,
+                header: headerProps,
+                checkMessageButtonTap: .nop
+            )
+        )
+    }
+}
