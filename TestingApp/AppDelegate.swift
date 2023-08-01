0a32813e47330529a5f77a4e69a66f80f0a86c5e
Yurii Dukhovnyi <yurii.dukhovnyi@glia.com>
2023-08-01 10:00:54 +0300

    Introduce process argument support
    Supporting process argument allows
    passing configuration url before
    installing the application and running
    AT. These changes should speedup and
    simplify the testing process.

--- TestingApp/AppDelegate.swift
+++ TestingApp/AppDelegate.swift
@@ -5,27 +5,17 @@ import GliaCoreSDK
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        handleProcessInfo()
+        handleSetAnimationsEnabled()
+    }
+
     func application(
         _ app: UIApplication,
         open url: URL,
-        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
     ) -> Bool {
-
-        guard
-            let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
-            let queryItems = components.queryItems
-        else {
-            debugPrint("URL is not valid. url='\(url.absoluteString)'.")
-            return false
-        }
-
-        guard let root = window?.rootViewController as? ViewController else {
-            return false
-        }
-
-        if components.host == "configure" {
-            root.updateConfiguration(with: queryItems)
-        }
+        handleConfigurationUrl(url: url)
         return true
     }
 
@@ -45,5 +35,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFailToRegisterForRemoteNotificationsWithError error: Error
     ) {
         debugPrint("💥 Failed to register for remote notifications. Error: \(error.localizedDescription)")
+    }
+
+    private  func handleProcessInfo() {
+        guard let configurationUrl = ProcessInfo.processInfo.environment["CONFIGURATION_URL"] else {
+            return
+        }
+
+        debugPrint(configurationUrl)
+
+        guard let url = URL(string: configurationUrl) else { return }
+        handleConfigurationUrl(url: url)
+    }
+
+    @discardableResult
+    private func handleConfigurationUrl(url: URL) -> Bool {
+        guard
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+            let queryItems = components.queryItems
+        else {
+            debugPrint("URL is not valid. url='\(url.absoluteString)'.")
+            return false
+        }
+
+        guard let root = window?.rootViewController as? ViewController else {
+            return false
+        }
+
+        if components.host == "configure" {
+            root.updateConfiguration(with: queryItems)
+        }
+
+        return true
+    }
+
+    private func handleSetAnimationsEnabled() {
+        let env = ProcessInfo.processInfo.environment
+        if env["SET_ANIMATION_ENABLED"] == "false" {
+            UIView.setAnimationsEnabled(false)
+        }
     }
 }
