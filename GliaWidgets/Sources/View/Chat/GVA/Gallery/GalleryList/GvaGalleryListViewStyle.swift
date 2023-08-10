cb3414353f2b12a8e5f25545fca378dd4224edde
Rasmus Tauts <rasmus.tauts@glia.com>
2023-08-10 16:27:18 +0300

    Apply Unified Customization to Gallery Card
    This PR adds unified customization to GVA Gallery card.
    
    MOB-2405

--- GliaWidgets/Sources/View/Chat/GVA/Gallery/GalleryList/GvaGalleryListViewStyle.swift
+++ GliaWidgets/Sources/View/Chat/GVA/Gallery/GalleryList/GvaGalleryListViewStyle.swift
@@ -24,4 +24,15 @@ public struct GvaGalleryListViewStyle {
             cardStyle: .initial
         )
     }
+
+    mutating func apply(
+        configuration: RemoteConfiguration.GvaGalleryCards?,
+        assetBuilder: RemoteConfiguration.AssetsBuilder
+    ) {
+        operatorImage.apply(configuration: configuration?.operatorImage)
+        cardStyle.apply(
+            configuration: configuration?.cardStyle,
+            assetBuilder: assetBuilder
+        )
+    }
 }
