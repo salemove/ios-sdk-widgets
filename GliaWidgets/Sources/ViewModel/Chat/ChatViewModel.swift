d51669260153cc50a5914753bdf8b08787104de9
Gerson Noboa <gerson.noboa@glia.com>
2023-08-16 13:32:49 +0300

    Remove unneeded alias for chat strings
    This alias has no effect in anything, so it was removed.
    
    MOB-2538

--- GliaWidgets/Sources/ViewModel/Chat/ChatViewModel.swift
+++ GliaWidgets/Sources/ViewModel/Chat/ChatViewModel.swift
@@ -815,9 +815,6 @@ extension ChatViewModel {
 }
 
 extension ChatViewModel {
-
-    typealias Strings = L10n.Chat
-
     enum Event {
         case viewDidLoad
         case messageTextChanged(String)
