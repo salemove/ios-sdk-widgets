258c01c62c96af77aa7b25db5af0f62653049490
Egor Egorov <egorov.egor@glia.com>
2023-08-23 21:40:04 +0300

    Postback button actions caching
    Caching mechanism for postback actions was added similar to caching pending messages for authenticated visitor. Now if authenticated visitor press postback button in chat transcript, chat engagement will be initiated and all postback actions will be sent, when engagement is established.
    
    MOB-2573

--- GliaWidgets/Sources/ViewModel/Chat/Data/OutgoingMessage.swift
+++ GliaWidgets/Sources/ViewModel/Chat/Data/OutgoingMessage.swift
@@ -1,23 +1,28 @@
 import Foundation
+import GliaCoreSDK
 
 class OutgoingMessage: Equatable {
     let id: String
     let content: String
     let files: [LocalFile]
+    let attachment: Attachment?
 
     init(
         id: String = UUID().uuidString,
         content: String,
-        files: [LocalFile] = []
+        files: [LocalFile] = [],
+        attachment: Attachment? = nil
     ) {
         self.id = id
         self.content = content
         self.files = files
+        self.attachment = attachment
     }
 
     static func == (lhs: OutgoingMessage, rhs: OutgoingMessage) -> Bool {
         lhs.id == rhs.id &&
         lhs.content == rhs.content &&
-        lhs.files == rhs.files
+        lhs.files == rhs.files &&
+        lhs.attachment == rhs.attachment
     }
 }
