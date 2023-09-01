0ef625debd482189787667c56b418641ee0940c3
Gerson Noboa <gerson.noboa@glia.com>
2023-09-01 16:28:35 +0300

    Update strings that don't need a translation layer
    There was a small amount of strings that I incorrectly identified as needing
    additional work to change, but it was just a matter of replacing because they
    are all internal and not exposed to the integrators.
    
    MOB-2507

--- GliaWidgets/Sources/ViewModel/Chat/ChatViewModel.swift
+++ GliaWidgets/Sources/ViewModel/Chat/ChatViewModel.swift
@@ -930,3 +930,83 @@ extension ChatViewModel {
         }
     }
 }
+
+extension ChatViewModel {
+    enum Event {
+        case viewDidLoad
+        case messageTextChanged(String)
+        case sendTapped
+        case removeUploadTapped(FileUpload)
+        case pickMediaTapped
+        case callBubbleTapped
+        case fileTapped(LocalFile)
+        case downloadTapped(FileDownload)
+        case choiceOptionSelected(ChatChoiceCardOption, String)
+        case chatScrolled(bottomReached: Bool)
+        case linkTapped(URL)
+        case customCardOptionSelected(
+            option: HtmlMetadata.Option,
+            messageId: MessageRenderer.Message.Identifier
+        )
+        case gvaButtonTapped(GvaOption)
+    }
+
+    enum Action {
+        case queue
+        case connected(name: String?, imageUrl: String?)
+        case transferring
+        case setMessageEntryEnabled(Bool)
+        case setChoiceCardInputModeEnabled(Bool)
+        case setMessageText(String)
+        case sendButtonHidden(Bool)
+        case pickMediaButtonEnabled(Bool)
+        case appendRows(Int, to: Int, animated: Bool)
+        case refreshRow(Int, in: Int, animated: Bool)
+        case refreshRows([Int], in: Int, animated: Bool)
+        case refreshSection(Int, animated: Bool = false)
+        case refreshAll
+        case scrollToBottom(animated: Bool)
+        case updateItemsUserImage(animated: Bool)
+        case addUpload(FileUpload)
+        case removeUpload(FileUpload)
+        case removeAllUploads
+        case presentMediaPicker(itemSelected: (AttachmentSourceItemKind) -> Void)
+        case offerMediaUpgrade(
+            SingleMediaUpgradeAlertConfiguration,
+            accepted: () -> Void,
+            declined: () -> Void
+        )
+        case showCallBubble(imageUrl: String?)
+        case setCallBubbleImage(imageUrl: String?)
+        case updateUnreadMessageIndicator(itemCount: Int)
+        case setUnreadMessageIndicatorImage(imageUrl: String?)
+        case setOperatorTypingIndicatorIsHiddenTo(Bool, _ isChatScrolledToBottom: Bool)
+        case setAttachmentButtonVisibility(MediaPickerButtonVisibility)
+        case fileUploadListPropsUpdated(SecureConversations.FileUploadListView.Props)
+        case quickReplyPropsUpdated(QuickReplyView.Props)
+    }
+
+    enum DelegateEvent {
+        case pickMedia(ObservableValue<MediaPickerEvent>)
+        case takeMedia(ObservableValue<MediaPickerEvent>)
+        case pickFile(ObservableValue<FilePickerEvent>)
+        case mediaUpgradeAccepted(
+            offer: CoreSdkClient.MediaUpgradeOffer,
+            answer: CoreSdkClient.AnswerWithSuccessBlock
+        )
+        case secureTranscriptUpgradedToLiveChat(ChatViewController)
+        case showFile(LocalFile)
+        case call
+    }
+
+    enum StartAction {
+        case startEngagement
+        case none
+    }
+
+    enum ChatType {
+        case secureTranscript
+        case authenticated
+        case nonAuthenticated
+    }
+}
