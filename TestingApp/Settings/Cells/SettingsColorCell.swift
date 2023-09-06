1251c2c731e21491525d258ee24515b07ea85503
Yurii Dukhovnyi <yurii.dukhovnyi@glia.com>
2023-09-07 00:00:26 +0300

    Introduce list queue interface
    Added mechanism for fetching queue id dynamically
    and assigning into settings.
    
    MOB-2594

--- TestingApp/Settings/Cells/SettingsColorCell.swift
+++ TestingApp/Settings/Cells/SettingsColorCell.swift
@@ -41,40 +41,15 @@ class SettingsColorCell: SettingsCell {
     }
 
     private func layout() {
-        contentView.addSubview(sampleView)
-        sampleView.translatesAutoresizingMaskIntoConstraints = false
+        contentStackView.addArrangedSubview(UIView())
+        contentStackView.addArrangedSubview(sampleView)
+        contentStackView.addArrangedSubview(rgbTextField)
+        contentStackView.addArrangedSubview(alphaTextField)
+
         var constraints = [NSLayoutConstraint](); defer { constraints.activate() }
         constraints += sampleView.match(value: 30)
-        constraints += sampleView.leadingAnchor.constraint(
-            greaterThanOrEqualTo: titleLabel.trailingAnchor,
-            constant: 20
-        )
-        constraints += sampleView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-
-        contentView.addSubview(rgbTextField)
-        rgbTextField.translatesAutoresizingMaskIntoConstraints = false
         constraints += rgbTextField.match(.width, value: 100)
-        constraints += rgbTextField.layoutInSuperview(
-            edges: .vertical,
-            insets: .init(top: 10, left: 0, bottom: 10, right: 0)
-        )
-        constraints += rgbTextField.leadingAnchor.constraint(
-            equalTo: sampleView.trailingAnchor,
-            constant: 10
-        )
-
-        contentView.addSubview(alphaTextField)
-        alphaTextField.translatesAutoresizingMaskIntoConstraints = false
         constraints += alphaTextField.match(.width, value: 50)
-        constraints += alphaTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-        constraints += alphaTextField.leadingAnchor.constraint(
-            equalTo: rgbTextField.trailingAnchor,
-            constant: 10
-        )
-        constraints += alphaTextField.layoutInSuperview(
-            edges: .trailing,
-            insets: UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 20)
-        )
     }
 
     @objc private func updateSample() {
--- TestingApp/Settings/Cells/SettingsSwitchCell.swift
+++ TestingApp/Settings/Cells/SettingsSwitchCell.swift
@@ -10,38 +10,7 @@ final class SettingsSwitchCell: SettingsCell {
     }
 
     private func layout() {
-        contentView.addSubview(switcher)
-        switcher.translatesAutoresizingMaskIntoConstraints = false
-        var constraints = [NSLayoutConstraint](); defer { constraints.activate() }
-        let insets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 20)
-        constraints += switcher.layoutInSuperview(edges: .vertical, insets: insets)
-        constraints += switcher.layoutInSuperview(edges: .trailing, insets: insets)
-        constraints += switcher.leadingAnchor.constraint(
-            equalTo: titleLabel.trailingAnchor,
-            constant: 10
-        )
-    }
-}
-
-final class SettingsSegmentedCell: SettingsCell {
-    let segmentedControl: UISegmentedControl
-
-    init(title: String, items: String...) {
-        self.segmentedControl = .init(items: items.map(NSString.init(string:)))
-        super.init(title: title)
-        layout()
-    }
-
-    private func layout() {
-        contentView.addSubview(segmentedControl)
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        var constraints = [NSLayoutConstraint](); defer { constraints.activate() }
-        let insets = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 20)
-        constraints += segmentedControl.layoutInSuperview(edges: .vertical, insets: insets)
-        constraints += segmentedControl.layoutInSuperview(edges: .trailing, insets: insets)
-        constraints += segmentedControl.leadingAnchor.constraint(
-            equalTo: titleLabel.trailingAnchor,
-            constant: 10
-        )
+        titleLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        contentStackView.addArrangedSubview(switcher)
     }
 }
