ae275ef3fa1489075554ba1cab51896753db5bf0
Rasmus Tauts <rasmus.tauts@glia.com>
2023-09-11 15:18:43 +0300

    Make screen sharing view ADA compliant

--- SnapshotTests/ScreenShareViewControllerLayoutTests.swift
+++ SnapshotTests/ScreenShareViewControllerLayoutTests.swift
@@ -4,14 +4,19 @@ import XCTest
 
 class ScreenShareViewControllerLayoutTests: SnapshotTestCase {
     func testScreenShareViewController() {
+        let theme = Theme()
+        let font = theme.font
         let props: CallVisualizer.ScreenSharingViewController.Props = .init(
             screenSharingViewProps: .init(
-                style: .mock(),
+                style: .mock(
+                    messageTextFont: font.header2,
+                    buttonTitleFont: font.bodyText
+                ),
                 header: .mock(
                     title: L10n.CallVisualizer.ScreenSharing.title,
                     backButton: .init(style: .mock(image: Asset.back.image))
                 ),
-                endScreenSharing: .mock()
+                endScreenSharing: .mock(style: .mock(titleFont: font.bodyText))
             )
         )
         let screenShareViewController = CallVisualizer.ScreenSharingViewController(props: props)
