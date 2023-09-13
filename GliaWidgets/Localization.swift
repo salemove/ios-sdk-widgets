08cd55d484611ece2c42f1e5562f06b9046a4490
Gerson Noboa <gerson.noboa@glia.com>
2023-09-13 11:05:12 +0300

    Introduce string providing mechanism
    The proposal is that the Core SDK will return to us a dictionary with the keys and
    values just as they come from the backend. This is also exactly how the strings
    are saved in the `Localizable.strings` file, accessed through the `Localization`
    enum. When the Core SDK is configured and is able to provide the dictionary, we
    save it to a StringProviding struct. When a string is accessed through the
    use of `Localization`, then `Localization` knows what is the key of the string,
    and also specifies a fallback in case reading from the file fails. So with this
    same key, we can go to the StringProviding, consult the dictionary, and if the
    string is not there, use the file fallback, and if not use the string fallback.
    This means that with this simple PR, once the Core SDK provides the dictionary,
    we support custom locales for the whole Widgets SDK with minimal effort. The
    alternative is to create a model which we need to keep in sync between Core and
    Widgets, which seems like a big mess.
    
    MOB-2282

--- GliaWidgets/Localization.swift
+++ GliaWidgets/Localization.swift
@@ -640,25 +640,4 @@ internal enum Localization {
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
 
-// MARK: - Implementation Details
-
-extension Localization {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
-    return String(format: format, locale: Locale.current, arguments: args)
-  }
-}
-
-// swiftlint:disable convenience_type
-private final class BundleToken {
-  static let bundle: Bundle = {
-    #if SWIFT_PACKAGE
-    return Bundle.module
-    #else
-    return Bundle(for: BundleToken.self)
-    #endif
-  }()
-}
-// swiftlint:enable convenience_type
-
 // swiftlint:enable all
