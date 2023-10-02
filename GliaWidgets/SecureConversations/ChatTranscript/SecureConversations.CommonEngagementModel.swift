057cc4f6704fa88ff3cddfc728d6e4ea9dd0c57d
Rasmus Tauts <rasmus.tauts@glia.com>
2023-10-02 14:12:16 +0300

    Display Live Observation alert before normal engagement
    This PR displays Live Observation confirmation alert before normal engagement.
    This alert can't be skipped and needs visitor's manual confirmation to
    enqueue.
    
    MOB-2678

--- GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.CommonEngagementModel.swift
+++ GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.CommonEngagementModel.swift
@@ -3,5 +3,7 @@ import Foundation
 protocol CommonEngagementModel: AnyObject {
     var engagementAction: EngagementViewModel.ActionCallback? { get set }
     var engagementDelegate: EngagementViewModel.DelegateCallback? { get set }
+    var hasViewAppeared: Bool { get }
     func event(_ event: EngagementViewModel.Event)
+    func setViewAppeared()
 }
