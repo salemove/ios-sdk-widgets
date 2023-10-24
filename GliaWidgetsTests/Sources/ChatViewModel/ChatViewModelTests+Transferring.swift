e3fef14e202eeab4861c67851d6c264e080c8863
Egor Egorov <egorov.egor@glia.com>
2023-10-24 14:54:09 +0300

    Make Glia.configuration private(set)

--- GliaWidgetsTests/Sources/ChatViewModel/ChatViewModelTests+Transferring.swift
+++ GliaWidgetsTests/Sources/ChatViewModel/ChatViewModelTests+Transferring.swift
@@ -20,7 +20,6 @@ extension ChatViewModelTests {
         interactorEnv.coreSdk.queueForEngagement = { _, _ in }
         interactorEnv.coreSdk.configureWithInteractor = { _ in }
         let interactorMock = Interactor.mock(environment: interactorEnv)
-//        interactorMock.isConfigurationPerformed = true
 
         var env = ChatViewModel.Environment.failing()
         env.fileManager.fileExistsAtPath = { _ in true }
