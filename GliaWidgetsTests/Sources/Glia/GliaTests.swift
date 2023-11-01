8f670fc40508f55db6f2ac93f4b485cb2a102ff9
Rasmus Tauts <rasmus.tauts@glia.com>
2023-11-01 13:16:35 +0200

    Show LO confirmation alert based on toggle
    This PR will take into account the possibility of toggling out LO
    confirmation alert, meaning that engagement can start without explicitly
    allowing LO. The toggle is fetch from the site information
    
    MOB-2707

--- GliaWidgetsTests/Sources/Glia/GliaTests.swift
+++ GliaWidgetsTests/Sources/Glia/GliaTests.swift
@@ -87,7 +87,7 @@ final class GliaTests: XCTestCase {
             completion(.success(()))
         }
         gliaEnv.coreSDKConfigurator.configureWithInteractor = { _ in }
-
+        gliaEnv.coreSdk.fetchSiteConfigurations = { _ in }
         gliaEnv.uuid = { .mock }
         gliaEnv.gcd.mainQueue.asyncIfNeeded = { callback in callback() }
 
@@ -172,6 +172,7 @@ final class GliaTests: XCTestCase {
         gliaEnv.coreSDKConfigurator.configureWithConfiguration = { _, completion in
             completion(.success(()))
         }
+        gliaEnv.callVisualizerPresenter = .init(presenter: { nil })
         gliaEnv.coreSDKConfigurator.configureWithInteractor = { _ in }
 
         let sdk = Glia(environment: gliaEnv)
