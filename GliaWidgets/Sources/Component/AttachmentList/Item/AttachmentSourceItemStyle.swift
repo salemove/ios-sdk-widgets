4ec6f25459e62ef18caee8f546ba6bd726144953
Rasmus Tauts <rasmus.tauts@glia.com>
2023-11-30 15:06:07 +0200

    FIx media picker last element alignment
    This PR fixes the alignment issue that was caused by explicit arrow
    direction declaration. Now the insets are correct.
    
    MOB-2857

--- GliaWidgets/Sources/Component/AttachmentList/Item/AttachmentSourceItemStyle.swift
+++ GliaWidgets/Sources/Component/AttachmentList/Item/AttachmentSourceItemStyle.swift
@@ -90,3 +90,40 @@ extension AttachmentSourceItemStyle {
         lhs.accessibility == rhs.accessibility
     }
 }
+
+#if DEBUG
+extension AttachmentSourceItemStyle {
+    public static func mock(with kind: AttachmentSourceItemKind) -> AttachmentSourceItemStyle {
+        let theme = Theme()
+        let titleFont = theme.font.bodyText
+        let titleColor = theme.color.baseDark
+        let iconColor = theme.color.baseDark
+        var title: String
+        var icon: UIImage
+
+        switch kind {
+        case .photoLibrary:
+            title = "Photo Library"
+            icon = Asset.photoLibraryIcon.image
+        case .takePhoto:
+            title = "Take phone"
+            icon = Asset.cameraIcon.image
+        case .browse:
+            title = "Browse"
+            icon = Asset.browseIcon.image
+        }
+
+        let item: AttachmentSourceItemStyle = .init(
+            kind: kind,
+            title: title,
+            titleFont: titleFont,
+            titleColor: titleColor,
+            icon: icon,
+            iconColor: iconColor,
+            accessibility: .init(isFontScalingEnabled: true)
+        )
+
+        return item
+    }
+}
+#endif
