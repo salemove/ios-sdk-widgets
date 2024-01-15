0f193c721754ca3ac1d3e276403e9cb2adc73970
Rasmus Tauts <rasmus.tauts@glia.com>
2024-01-15 16:32:38 +0200

    Improve LO indicator showing logic
    This PR improves the logic of showing LO indicator. It now checks if both
    mobile observation and mobile observation indication are enabled.
    
    MOB-2986

--- GliaWidgetsTests/CallVisualizer/CallVisualizerTests+LO.swift
+++ GliaWidgetsTests/CallVisualizer/CallVisualizerTests+LO.swift
@@ -11,6 +11,7 @@ extension CallVisualizerTests {
         gliaEnv.conditionalCompilation.isDebug = { true }
         gliaEnv.coreSdk.createLogger = { _ in .mock }
         let site = try CoreSdkClient.Site.mock(
+            mobileObservationEnabled: true,
             mobileConfirmDialogEnabled: false,
             mobileObservationIndicationEnabled: true
         )
@@ -38,6 +39,78 @@ extension CallVisualizerTests {
         XCTAssertEqual(calls, [.presentSnackBar])
     }
 
+    func testLiveObservationIndicationIsDisabledOnEngagementRequest() throws {
+        enum Call { case presentSnackBar }
+        var calls: [Call] = []
+
+        var gliaEnv = Glia.Environment.failing
+        gliaEnv.conditionalCompilation.isDebug = { true }
+        gliaEnv.coreSdk.createLogger = { _ in .mock }
+        let site = try CoreSdkClient.Site.mock(
+            mobileObservationEnabled: true,
+            mobileConfirmDialogEnabled: false,
+            mobileObservationIndicationEnabled: false
+        )
+        gliaEnv.coreSdk.fetchSiteConfigurations = { completion in
+            completion(.success(site))
+        }
+        gliaEnv.callVisualizerPresenter = .init(presenter: { nil })
+        gliaEnv.gcd.mainQueue.async = { $0() }
+        var interactable: CoreSdkClient.Interactable?
+        gliaEnv.coreSDKConfigurator.configureWithInteractor = { interactor in
+            interactable = interactor
+        }
+        gliaEnv.coreSDKConfigurator.configureWithConfiguration = { _, completion in
+            completion(.success(()))
+        }
+        gliaEnv.snackBar.present = { _, _, _, _, _, _, _ in
+            calls.append(.presentSnackBar)
+        }
+        let sdk = Glia(environment: gliaEnv)
+        try sdk.configure(with: .mock(), theme: .mock(), completion: { _ in })
+        sdk.callVisualizer.delegate?(.visitorCodeIsRequested)
+
+        interactable?.onEngagementRequest({ _, _, _ in })
+
+        XCTAssertEqual(calls, [])
+    }
+
+    func testLiveObservationIsDisabledOnEngagementRequest() throws {
+        enum Call { case presentSnackBar }
+        var calls: [Call] = []
+
+        var gliaEnv = Glia.Environment.failing
+        gliaEnv.conditionalCompilation.isDebug = { true }
+        gliaEnv.coreSdk.createLogger = { _ in .mock }
+        let site = try CoreSdkClient.Site.mock(
+            mobileObservationEnabled: false,
+            mobileConfirmDialogEnabled: false,
+            mobileObservationIndicationEnabled: true
+        )
+        gliaEnv.coreSdk.fetchSiteConfigurations = { completion in
+            completion(.success(site))
+        }
+        gliaEnv.callVisualizerPresenter = .init(presenter: { nil })
+        gliaEnv.gcd.mainQueue.async = { $0() }
+        var interactable: CoreSdkClient.Interactable?
+        gliaEnv.coreSDKConfigurator.configureWithInteractor = { interactor in
+            interactable = interactor
+        }
+        gliaEnv.coreSDKConfigurator.configureWithConfiguration = { _, completion in
+            completion(.success(()))
+        }
+        gliaEnv.snackBar.present = { _, _, _, _, _, _, _ in
+            calls.append(.presentSnackBar)
+        }
+        let sdk = Glia(environment: gliaEnv)
+        try sdk.configure(with: .mock(), theme: .mock(), completion: { _ in })
+        sdk.callVisualizer.delegate?(.visitorCodeIsRequested)
+
+        interactable?.onEngagementRequest({ _, _, _ in })
+
+        XCTAssertEqual(calls, [])
+    }
+
     func testLiveObservationIndicatorIsPresentedOnEngagementRestore() throws {
         enum Call { case presentSnackBar }
         var calls: [Call] = []
@@ -49,6 +122,7 @@ extension CallVisualizerTests {
         }
         gliaEnv.coreSdk.createLogger = { _ in .mock }
         let site = try CoreSdkClient.Site.mock(
+            mobileObservationEnabled: true,
             mobileConfirmDialogEnabled: false,
             mobileObservationIndicationEnabled: true
         )
@@ -68,5 +142,71 @@ extension CallVisualizerTests {
         try sdk.configure(with: .mock(), theme: .mock(), completion: { _ in })
 
         XCTAssertEqual(calls, [.presentSnackBar])
+    }
+
+    func testLiveObservationIndicationIsDisabledOnEngagementRestore() throws {
+        enum Call { case presentSnackBar }
+        var calls: [Call] = []
+
+        var gliaEnv = Glia.Environment.failing
+        gliaEnv.conditionalCompilation.isDebug = { true }
+        gliaEnv.snackBar.present = { _, _, _, _, _, _, _ in
+            calls.append(.presentSnackBar)
+        }
+        gliaEnv.coreSdk.createLogger = { _ in .mock }
+        let site = try CoreSdkClient.Site.mock(
+            mobileObservationEnabled: true,
+            mobileConfirmDialogEnabled: true,
+            mobileObservationIndicationEnabled: false
+        )
+        gliaEnv.coreSdk.fetchSiteConfigurations = { completion in
+            completion(.success(site))
+        }
+        gliaEnv.callVisualizerPresenter = .init(presenter: { nil })
+        gliaEnv.gcd.mainQueue.asyncIfNeeded = { $0() }
+        gliaEnv.coreSDKConfigurator.configureWithInteractor = { _ in }
+        let sdk = Glia(environment: gliaEnv)
+        sdk.environment.coreSDKConfigurator.configureWithConfiguration = { _, completion in
+            sdk.environment.coreSdk.getCurrentEngagement = {
+                .mock(source: .callVisualizer)
+            }
+            completion(.success(()))
+        }
+        try sdk.configure(with: .mock(), theme: .mock(), completion: { _ in })
+
+        XCTAssertEqual(calls, [])
+    }
+
+    func testLiveObservationIsDisabledOnEngagementRestore() throws {
+        enum Call { case presentSnackBar }
+        var calls: [Call] = []
+
+        var gliaEnv = Glia.Environment.failing
+        gliaEnv.conditionalCompilation.isDebug = { true }
+        gliaEnv.snackBar.present = { _, _, _, _, _, _, _ in
+            calls.append(.presentSnackBar)
+        }
+        gliaEnv.coreSdk.createLogger = { _ in .mock }
+        let site = try CoreSdkClient.Site.mock(
+            mobileObservationEnabled: false,
+            mobileConfirmDialogEnabled: true,
+            mobileObservationIndicationEnabled: true
+        )
+        gliaEnv.coreSdk.fetchSiteConfigurations = { completion in
+            completion(.success(site))
+        }
+        gliaEnv.callVisualizerPresenter = .init(presenter: { nil })
+        gliaEnv.gcd.mainQueue.asyncIfNeeded = { $0() }
+        gliaEnv.coreSDKConfigurator.configureWithInteractor = { _ in }
+        let sdk = Glia(environment: gliaEnv)
+        sdk.environment.coreSDKConfigurator.configureWithConfiguration = { _, completion in
+            sdk.environment.coreSdk.getCurrentEngagement = {
+                .mock(source: .callVisualizer)
+            }
+            completion(.success(()))
+        }
+        try sdk.configure(with: .mock(), theme: .mock(), completion: { _ in })
+
+        XCTAssertEqual(calls, [])
     }
 }
