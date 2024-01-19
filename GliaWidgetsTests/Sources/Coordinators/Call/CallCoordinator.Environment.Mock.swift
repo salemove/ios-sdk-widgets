4d147c5a4a67d140dcd78fcf6b7e8f8c2a410e24
Gerson Noboa <gerson.noboa@glia.com>
2024-01-19 14:58:48 +0200

    Add unit tests for CallCoordinator
    Correct actions and creation of view controllers in the coordinator has been
    covered with unit tests.
    
    MOB-2920

---
+++ GliaWidgetsTests/Sources/Coordinators/Call/CallCoordinator.Environment.Mock.swift
@@ -0,0 +1,31 @@
+import Foundation
+@testable import GliaWidgets
+
+extension CallCoordinator.Environment {
+    static let mock = Self(
+        fetchFile: { engagementFile, progress, completion in },
+        downloadSecureFile: { file, progress, completion in .mock },
+        uploadFileToEngagement: { file, progress, completion in },
+        fileManager: .mock,
+        data: .mock,
+        date: { .mock },
+        gcd: .mock,
+        createThumbnailGenerator: { .mock },
+        createFileDownload: { file, storage, environment in .mock() },
+        fromHistory: { false },
+        fetchSiteConfigurations: { completion in },
+        getCurrentEngagement: { .mock() },
+        timerProviding: .mock,
+        submitSurveyAnswer: { answers, surveyId, engagementId, completion in },
+        uuid: { .mock },
+        uiApplication: .mock,
+        uiScreen: .mock,
+        notificationCenter: .mock,
+        fetchChatHistory: { completion in },
+        createFileUploadListModel: { environment in .mock() },
+        createSendMessagePayload: { content, attachment in .mock() },
+        proximityManager: .mock,
+        log: .mock,
+        snackBar: .mock
+    )
+}
