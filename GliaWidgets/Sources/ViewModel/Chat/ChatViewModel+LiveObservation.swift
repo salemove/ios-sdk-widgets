2b90a9919186fe72bfd8bf508ec60f4e106fe6ab
Yurii Dukhovnyi <yurii.dukhovnyi@glia.com>
2024-03-14 22:10:15 +0200

    Introduced skipLOConfirmations to forcibly skip LO ack
    In case restarting engagement happen after authentication
    mobile SDK should ignore site configuration and don't
    show LO ack again.
    
    MOB-3095

--- GliaWidgets/Sources/ViewModel/Chat/ChatViewModel+LiveObservation.swift
+++ GliaWidgets/Sources/ViewModel/Chat/ChatViewModel+LiveObservation.swift
@@ -4,6 +4,10 @@ extension ChatViewModel {
     func showSnackBarIfNeeded() {
         guard siteConfiguration?.mobileObservationEnabled == true else { return }
         guard siteConfiguration?.mobileObservationIndicationEnabled == true else { return }
+        // If engagement has been restarted, visitor seen the acknowledgement and snackbar, so skip
+        // it for restarting/restoring engagement.
+        guard interactor.skipLOConfirmations == false else { return }
+
         action?(.showSnackBarView)
     }
 }
