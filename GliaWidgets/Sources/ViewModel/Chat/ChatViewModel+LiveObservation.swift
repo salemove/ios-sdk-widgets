93c09ce508fb1d9c720a26f73cb44ce1602fcdb8
Yurii Dukhovnyi <yurii.dukhovnyi@glia.com>
2024-03-21 09:58:22 +0200

    Add public doc for Behavior.allowedDuringEngagement and renaming

--- GliaWidgets/Sources/ViewModel/Chat/ChatViewModel+LiveObservation.swift
+++ GliaWidgets/Sources/ViewModel/Chat/ChatViewModel+LiveObservation.swift
@@ -6,7 +6,7 @@ extension ChatViewModel {
         guard siteConfiguration?.mobileObservationIndicationEnabled == true else { return }
         // If engagement has been restarted, visitor seen the acknowledgement and snackbar, so skip
         // it for restarting/restoring engagement.
-        guard interactor.skipLOConfirmations == false else { return }
+        guard interactor.skipLiveObservationConfirmations == false else { return }
 
         action?(.showSnackBarView)
     }
--- GliaWidgets/Sources/ViewModel/EngagementViewModel.swift
+++ GliaWidgets/Sources/ViewModel/EngagementViewModel.swift
@@ -167,7 +167,7 @@ class EngagementViewModel: CommonEngagementModel {
                 guard let self else { return }
                 switch result {
                 case let .success(site):
-                    if site.mobileConfirmDialogEnabled == false || self.interactor.skipLOConfirmations {
+                    if site.mobileConfirmDialogEnabled == false || self.interactor.skipLiveObservationConfirmations {
                         self.enqueue(mediaType: mediaType)
                     } else {
                         self.showLiveObservationConfirmation(in: mediaType)
