cdc37edcd3300a426541bbeb9af0a32d9eb506e1
Gerson Noboa <gerson.noboa@glia.com>
2024-04-12 14:16:32 +0300

    Add tests for CallVisualizerCoordinator
    The delegates inside the coordinators are not tested because each coordinator
    itself tests the delegate methods functioning through their own set of tests.
    Also, the coordinators in this class are all private, so testing it from outside
    would require moving the coordinators to internal properties, which I would say
    is not desirable.
    
    MOB-2917

---
+++ GliaWidgetsTests/Sources/CallVisualizer/Coordinator/CallVisualizer.Coordinator.Environment.Mock.swift
@@ -0,0 +1,30 @@
+import Foundation
+@testable import GliaWidgets
+
+extension CallVisualizer.Coordinator.Environment {
+    static let mock: Self = .init(
+        data: .mock,
+        uuid: { .mock },
+        gcd: .mock,
+        imageViewCache: .mock,
+        uiApplication: .mock,
+        uiScreen: .mock,
+        uiDevice: .mock,
+        notificationCenter: .mock,
+        viewFactory: .mock(),
+        presenter: .topViewController(application: .mock),
+        bundleManaging: .live,
+        screenShareHandler: .mock,
+        timerProviding: .mock,
+        requestVisitorCode: { completion in .mock },
+        audioSession: .mock,
+        date: { .mock },
+        engagedOperator: { .mock() },
+        eventHandler: { event in },
+        orientationManager: .mock(),
+        proximityManager: .mock,
+        log: .mock,
+        fetchSiteConfigurations: { completion in },
+        snackBar: .mock
+    )
+}
