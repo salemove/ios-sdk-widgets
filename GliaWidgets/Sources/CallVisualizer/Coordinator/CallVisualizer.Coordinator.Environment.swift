de520428b7f4021647244fff33bdb9141c7596c3
Rasmus Tauts <rasmus.tauts@glia.com>
2024-06-13 16:22:01 +0300

    Implement CallVisualizer environment initialization methods

--- GliaWidgets/Sources/CallVisualizer/Coordinator/CallVisualizer.Coordinator.Environment.swift
+++ GliaWidgets/Sources/CallVisualizer/Coordinator/CallVisualizer.Coordinator.Environment.swift
@@ -30,3 +30,40 @@ extension CallVisualizer.Coordinator {
         var alertManager: AlertManager
     }
 }
+
+extension CallVisualizer.Coordinator.Environment {
+    static func create(
+        with environment: CallVisualizer.Environment,
+        viewFactory: ViewFactory,
+        eventHandler: @escaping (CallVisualizer.Coordinator.DelegateEvent) -> Void
+    ) -> Self {
+        .init(
+            data: environment.data,
+            uuid: environment.uuid,
+            gcd: environment.gcd,
+            imageViewCache: environment.imageViewCache,
+            uiApplication: environment.uiApplication,
+            uiScreen: environment.uiScreen,
+            uiDevice: environment.uiDevice,
+            notificationCenter: environment.notificationCenter,
+            viewFactory: viewFactory,
+            presenter: environment.callVisualizerPresenter,
+            bundleManaging: environment.bundleManaging,
+            screenShareHandler: environment.screenShareHandler,
+            timerProviding: environment.timerProviding,
+            requestVisitorCode: environment.requestVisitorCode,
+            audioSession: environment.audioSession,
+            date: environment.date,
+            engagedOperator: environment.engagedOperator,
+            eventHandler: eventHandler,
+            orientationManager: environment.orientationManager,
+            proximityManager: environment.proximityManager,
+            log: environment.log,
+            interactorProviding: environment.interactorProviding(),
+            fetchSiteConfigurations: environment.fetchSiteConfigurations,
+            snackBar: environment.snackBar,
+            cameraDeviceManager: environment.cameraDeviceManager,
+            alertManager: environment.alertManager
+        )
+    }
+}
