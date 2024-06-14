e474630e5a2acdcf8a4229d6759ee9202795d9a6
Rasmus Tauts <rasmus.tauts@glia.com>
2024-06-14 11:00:11 +0300

    Implement Bubble environments initializations methods

--- GliaWidgets/Sources/Component/Bubble/BubbleView.swift
+++ GliaWidgets/Sources/Component/Bubble/BubbleView.swift
@@ -172,12 +172,3 @@ class BubbleView: BaseView {
         gesture.setTranslation(.zero, in: self)
     }
 }
-
-extension BubbleView {
-    struct Environment {
-        var data: FoundationBased.Data
-        var uuid: () -> UUID
-        var gcd: GCD
-        var imageViewCache: ImageView.Cache
-    }
-}
