5013cb4a3851f2b4f4714496478f1908f8329dbc
Rasmus Tauts <rasmus.tauts@glia.com>
2024-06-17 16:02:12 +0300

    Implement Call-related environments initialization methods

--- GliaWidgets/Sources/View/Call/CallView.swift
+++ GliaWidgets/Sources/View/Call/CallView.swift
@@ -523,16 +523,3 @@ extension CallView {
         )
     }
 }
-
-extension CallView {
-    struct Environment {
-        var data: FoundationBased.Data
-        var uuid: () -> UUID
-        var gcd: GCD
-        var imageViewCache: ImageView.Cache
-        var timerProviding: FoundationBased.Timer.Providing
-        var uiApplication: UIKitBased.UIApplication
-        var uiScreen: UIKitBased.UIScreen
-        var uiDevice: UIKitBased.UIDevice
-    }
-}
