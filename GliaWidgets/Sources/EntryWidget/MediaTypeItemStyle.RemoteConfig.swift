2f9941fdca61af2e82aa8731435b6cdccbb8d095
Rasmus Tauts <rasmus.tauts@glia.com>
2024-10-03 13:17:19 +0300

    Add Unified Customization to EntryWidget
    Add Unified Customization to EntryWidget, as well as add missing UI
    elements to accompany the customization options
    
    MOB-3592

---
+++ GliaWidgets/Sources/EntryWidget/MediaTypeItemStyle.RemoteConfig.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+extension EntryWidgetStyle.MediaTypeItemStyle {
+    mutating func apply(
+        configuration: RemoteConfiguration.MediaTypeItem?,
+        assetsBuilder: RemoteConfiguration.AssetsBuilder
+    ) {
+        configuration?.background?.color.unwrap {
+            switch $0.type {
+            case .fill:
+                $0.value
+                    .map { UIColor(hex: $0) }
+                    .first
+                    .unwrap { backgroundColor = .fill(color: $0) }
+            case .gradient:
+                let colors = $0.value.convertToCgColors()
+                backgroundColor = .gradient(colors: colors)
+            }
+        }
+
+        configuration?.iconColor.unwrap {
+            switch $0.type {
+            case .fill:
+                $0.value
+                    .map { UIColor(hex: $0) }
+                    .first
+                    .unwrap { backgroundColor = .fill(color: $0) }
+            case .gradient:
+                let colors = $0.value.convertToCgColors()
+                backgroundColor = .gradient(colors: colors)
+            }
+        }
+
+        configuration?.title?.foreground?.value
+            .map { UIColor(hex: $0) }
+            .first
+            .unwrap { titleColor = $0 }
+
+        UIFont.convertToFont(
+            uiFont: assetsBuilder.fontBuilder(configuration?.title?.font),
+            textStyle: messageTextStyle
+        ).unwrap { titleFont = $0 }
+
+        configuration?.message?.foreground?.value
+            .map { UIColor(hex: $0) }
+            .first
+            .unwrap { messageColor = $0 }
+
+        UIFont.convertToFont(
+            uiFont: assetsBuilder.fontBuilder(configuration?.message?.font),
+            textStyle: messageTextStyle
+        ).unwrap { messageFont = $0 }
+    }
+}
