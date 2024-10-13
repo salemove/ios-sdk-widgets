307627993b2b6a480897c21af045ab11a12ecdd9
Yevhen Kyivskyi <yevhen.kyivskyi@glia.com>
2024-10-13 16:34:54 +0200

    Integrate queues monitor and engagement launcher into EntryWidget

---
+++ GliaWidgets/Sources/EntryWidget/EntryWidget.Environment.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension EntryWidget {
+    struct Environment {
+        var queuesMonitor: QueuesMonitor
+        var engagementLauncher: EngagementLauncher
+        var theme: Theme
+    }
+}
