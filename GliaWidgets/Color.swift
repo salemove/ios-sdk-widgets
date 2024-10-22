38f7c826e22139514cc4e10cd49bfe864c4dbce3
Igor Kravchenko <igman2005@gmail.com>
2024-10-22 19:58:36 +0300

    Update default global colors according to color palette
    Update default global colors according to Mobile Color Tokens palette
    
    MOB-3710

--- GliaWidgets/Color.swift
+++ GliaWidgets/Color.swift
@@ -2,11 +2,11 @@ import UIKit
 
 enum Color {
     static let primary = UIColor(hex: 0x0F6BFF) // blue
-    static let secondary = UIColor(hex: 0x00B233) // green
+    static let secondary = UIColor(hex: 0x18901C) // green
     static let baseLight = UIColor(hex: 0xFFFFFF) // white
-    static let systemNegative = UIColor(hex: 0xD11149) // red
-    static let baseNormal = UIColor(hex: 0x6C7683) // grey
-    static let baseShade = UIColor(hex: 0x6C7683, alpha: 0.5)
+    static let systemNegative = UIColor(hex: 0xBC0F42) // red
+    static let baseNormal = UIColor(hex: 0x616A75) // gray
+    static let baseShade = UIColor(hex: 0xB6BBC1) // mid-gray
     static let baseDark = UIColor(hex: 0x2C0735) // purple
-    static let baseNeutral = UIColor(hex: 0xF3F3F3) // light gray
+    static let baseNeutral = UIColor(hex: 0xF7F7F7) // light gray
 }
