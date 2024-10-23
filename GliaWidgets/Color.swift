61bfc03cb020c686c69bc2d669d464cb81036271
Igor Kravchenko <igman2005@gmail.com>
2024-10-23 11:38:07 +0300

    fixup! Update default global colors according to color palette
    Update comments.

--- GliaWidgets/Color.swift
+++ GliaWidgets/Color.swift
@@ -5,8 +5,8 @@ enum Color {
     static let secondary = UIColor(hex: 0x18901C) // green
     static let baseLight = UIColor(hex: 0xFFFFFF) // white
     static let systemNegative = UIColor(hex: 0xBC0F42) // red
-    static let baseNormal = UIColor(hex: 0x616A75) // grey
-    static let baseShade = UIColor(hex: 0xB6BBC1)
+    static let baseNormal = UIColor(hex: 0x616A75) // gray
+    static let baseShade = UIColor(hex: 0xB6BBC1) // mid-gray
     static let baseDark = UIColor(hex: 0x2C0735) // purple
     static let baseNeutral = UIColor(hex: 0xF7F7F7) // light gray
 }
