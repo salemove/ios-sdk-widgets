cc2637902843884db0007c1965ba06004faa6364
Egor Egorov <egorov.egor@glia.com>
2024-10-25 12:24:29 +0300

    Leave Current Conversation dialog UI
    This commit introduces:
     - new alert dialog called Leave Current Conversation;
     - style for the same dialog
    
    MOB-3734

--- GliaWidgets/Sources/AlertManager/Alert/AlertViewController+Confirmation.swift
+++ GliaWidgets/Sources/AlertManager/Alert/AlertViewController+Confirmation.swift
@@ -1,7 +1,90 @@
 import UIKit
 
 extension AlertViewController {
+    func makeLeaveConversationAlertView(
+        with conf: ConfirmationAlertConfiguration,
+        accessibilityIdentifier: String,
+        confirmed: @escaping () -> Void
+    ) -> AlertView {
+        let alertView = makeAlertView(
+            with: conf,
+            accessibilityIdentifier: accessibilityIdentifier,
+            confirmed: confirmed
+        )
+        let alertStyle = viewFactory.theme.alert
+        var negativeButtonStyle = alertStyle.negativeNeutralAction
+        var positiveButtonStyle = alertStyle.positiveAction
+
+        if conf.switchButtonBackgroundColors {
+            negativeButtonStyle = alertStyle.positiveAction
+            positiveButtonStyle = alertStyle.negativeNeutralAction
+        }
+
+        negativeButtonStyle.title = conf.negativeTitle
+        positiveButtonStyle.title = conf.positiveTitle
+
+        let confirmButton: ActionButton = ActionButton(
+            props: .init(
+                style: negativeButtonStyle,
+                tap: .init { [weak self] in self?.dismiss(animated: true) { confirmed() } },
+                accessibilityIdentifier: "alert_negative_button"
+            )
+        )
+        let declineButton = ActionButton(
+            props: ActionButton.Props(
+                style: positiveButtonStyle,
+                tap: .init { [weak self] in self?.dismiss(animated: true) },
+                accessibilityIdentifier: "alert_positive_button"
+            )
+        )
+        alertView.addActionView(declineButton)
+        alertView.addActionView(confirmButton)
+        return alertView
+    }
+
     func makeConfirmationAlertView(
+        with conf: ConfirmationAlertConfiguration,
+        accessibilityIdentifier: String,
+        confirmed: @escaping () -> Void
+    ) -> AlertView {
+        let alertView = makeAlertView(
+            with: conf,
+            accessibilityIdentifier: accessibilityIdentifier,
+            confirmed: confirmed
+        )
+        let alertStyle = viewFactory.theme.alert
+        var negativeButtonStyle = alertStyle.negativeAction
+        var positiveButtonStyle = alertStyle.positiveAction
+
+        if conf.switchButtonBackgroundColors {
+            negativeButtonStyle = alertStyle.positiveAction
+            positiveButtonStyle = alertStyle.negativeAction
+        }
+
+        negativeButtonStyle.title = conf.negativeTitle
+        positiveButtonStyle.title = conf.positiveTitle
+
+        let declineButton: ActionButton = ActionButton(
+            props: .init(
+                style: negativeButtonStyle,
+                tap: .init { [weak self] in self?.dismiss(animated: true) },
+                accessibilityIdentifier: "alert_negative_button"
+            )
+        )
+        let confirmButton = ActionButton(
+            props: ActionButton.Props(
+                style: positiveButtonStyle,
+                tap: .init { [weak self] in self?.dismiss(animated: true) { confirmed() } },
+                accessibilityIdentifier: "alert_positive_button"
+            )
+        )
+
+        alertView.addActionView(declineButton)
+        alertView.addActionView(confirmButton)
+        return alertView
+    }
+
+    private func makeAlertView(
         with conf: ConfirmationAlertConfiguration,
         accessibilityIdentifier: String,
         confirmed: @escaping () -> Void
@@ -11,37 +94,6 @@ extension AlertViewController {
         alertView.message = conf.message
         alertView.showsPoweredBy = conf.showsPoweredBy
         alertView.accessibilityIdentifier = accessibilityIdentifier
-
-        let alertStyle = viewFactory.theme.alert
-
-        var negativeButtonStyle = alertStyle.negativeAction
-        var positiveButtonStyle = alertStyle.positiveAction
-
-        if conf.switchButtonBackgroundColors {
-            negativeButtonStyle = alertStyle.positiveAction
-            positiveButtonStyle = alertStyle.negativeAction
-        }
-
-        negativeButtonStyle.title = conf.negativeTitle ?? ""
-        positiveButtonStyle.title = conf.positiveTitle ?? ""
-
-        let negativeButton: ActionButton = ActionButton(
-            props: .init(
-                style: negativeButtonStyle,
-                tap: .init { [weak self] in self?.dismiss(animated: true) },
-                accessibilityIdentifier: "alert_negative_button"
-            )
-        )
-        let positiveButton = ActionButton(
-            props: ActionButton.Props(
-                style: positiveButtonStyle,
-                tap: .init { [weak self] in self?.dismiss(animated: true) { confirmed() } },
-                accessibilityIdentifier: "alert_positive_button"
-            )
-        )
-
-        alertView.addActionView(negativeButton)
-        alertView.addActionView(positiveButton)
 
         return alertView
     }
