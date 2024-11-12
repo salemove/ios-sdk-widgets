45b6d5896b9377bfcece8e63c6c148c30c6839e0
Rasmus Tauts <rasmus.tauts@glia.com>
2024-11-12 16:29:59 +0200

    Add logs and telemetry for Entry Widget
    This PR adds logs to EntryWidget, and aligning iOS with Android logs.
    Because logger was added as a dependency to QueueMonitor, it was necessary
    to move queueMonitor from Glia environment to Glia's class property due to
    Logger being initialized during GliaSDK initialization. Because of that
    some changes can be seen in tests as well.
    
    MOB-3481

--- GliaWidgetsTests/Sources/QueuesMonitor/QueuesMonitorTests.swift
+++ GliaWidgetsTests/Sources/QueuesMonitor/QueuesMonitorTests.swift
@@ -29,11 +29,14 @@ class QueuesMonitorTests: XCTestCase {
     // MARK: Fetch and Motitor queues
     func test_fetchAndMonitorQueuesWithQueuesUpdatesWithQueuesList() {
         var envCalls: [Call] = []
-
-        let mockQueueId = UUID().uuidString
+        let expectedLogMessage = "Setting up queues. 1 out of 1 queues provided by an integrator match with site queues."
+        var receivedLogMessage = ""
+        let mockQueueId = "mock_queue_id"
         let expectedObservedQueues = [Queue.mock(id: mockQueueId)]
         let mockQueues = [expectedObservedQueues[0], Queue.mock(id: UUID().uuidString)]
-
+        monitor.environment.logger.infoClosure = { logMessage, _, _, _ in
+            receivedLogMessage = logMessage as? String ?? ""
+        }
         monitor.environment.listQueues = { completion in
             envCalls.append(.listQueues)
             completion(mockQueues, nil)
@@ -57,14 +60,22 @@ class QueuesMonitorTests: XCTestCase {
 
         XCTAssertEqual(receivedQueues, expectedObservedQueues)
         XCTAssertEqual(envCalls, [.listQueues, .subscribeForQueuesUpdates])
+        XCTAssertEqual(receivedLogMessage, expectedLogMessage)
     }
 
     func test_fetchAndMonitorQueuesWithWrongQueueIdsReturnsUpdatesWithDefaultQueue() {
         var envCalls: [Call] = []
-
+        let expectedLogMessages = [
+            "Setting up queues. 0 out of 1 queues provided by an integrator match with site queues.",
+            "Setting up queues. Integrator specified an empty list of queues.",
+            "Setting up queues. Using 1 default queues."
+        ]
+        var receivedLogMessage: [String] = []
         let expectedObservedQueues = [Queue.mock(isDefault: true)]
         let mockQueues = [expectedObservedQueues[0], Queue.mock()]
-
+        monitor.environment.logger.infoClosure = { logMessage, _, _, _ in
+            receivedLogMessage.append(logMessage as? String ?? "")
+        }
         monitor.environment.listQueues = { completion in
             envCalls.append(.listQueues)
             completion(mockQueues, nil)
@@ -87,13 +98,17 @@ class QueuesMonitorTests: XCTestCase {
 
         XCTAssertEqual(receivedQueues, expectedObservedQueues)
         XCTAssertEqual(envCalls, [.listQueues, .subscribeForQueuesUpdates])
+        XCTAssertEqual(expectedLogMessages, receivedLogMessage)
     }
 
     func test_fetchAndMonitorQueuesListQueuesReturnsError() {
         var envCalls: [Call] = []
-
+        let expectedErrorLog = "Setting up queues. Failed to get site queues: mock"
+        var receivedErrorLog = ""
         let expectedError = CoreSdkClient.SalemoveError.mock()
-
+        monitor.environment.logger.errorClosure = { logMessage, _, _, _ in
+            receivedErrorLog = logMessage as? String ?? ""
+        }
         monitor.environment.listQueues = { completion in
             envCalls.append(.listQueues)
             completion(nil, expectedError)
@@ -116,12 +131,13 @@ class QueuesMonitorTests: XCTestCase {
 
         XCTAssertEqual(receivedError, expectedError)
         XCTAssertEqual(envCalls, [.listQueues])
+        XCTAssertEqual(expectedErrorLog, receivedErrorLog)
     }
 
     func test_fetchAndMonitorQueuesWithQueuesAndReceiveUpdatedQueueSuccess() {
         var envCalls: [Call] = []
 
-        let mockQueueId = UUID().uuidString
+        let mockQueueId = "mock_queue_id"
         let expectedObservedQueue = Queue.mock(id: mockQueueId, status: .closed)
         let expectedUpdatedQueue = Queue.mock(id: mockQueueId, status: .open)
         let mockQueues = [expectedObservedQueue, Queue.mock(id: UUID().uuidString)]
@@ -159,7 +175,7 @@ class QueuesMonitorTests: XCTestCase {
     func test_fetchAndMonitorQueuesWithQueuesStopsPreviousMonitoring() {
         var envCalls: [Call] = []
 
-        let mockQueueId = UUID().uuidString
+        let mockQueueId = "mock_queue_id"
         let expectedObservedQueue = Queue.mock(id: mockQueueId, status: .closed)
         let expectedUpdatedQueue = Queue.mock(id: mockQueueId, status: .open)
         let mockQueues = [expectedObservedQueue, Queue.mock(id: UUID().uuidString)]
@@ -205,7 +221,7 @@ class QueuesMonitorTests: XCTestCase {
     func test_fetchAndMonitorQueuesWithQueuesStopsPreviousMonitoringFailed() {
         var envCalls: [Call] = []
 
-        let mockQueueId = UUID().uuidString
+        let mockQueueId = "mock_queue_id"
         let expectedObservedQueue = Queue.mock(id: mockQueueId, status: .closed)
         let expectedUpdatedQueue = Queue.mock(id: mockQueueId, status: .open)
         let mockQueues = [expectedObservedQueue, Queue.mock(id: UUID().uuidString)]
