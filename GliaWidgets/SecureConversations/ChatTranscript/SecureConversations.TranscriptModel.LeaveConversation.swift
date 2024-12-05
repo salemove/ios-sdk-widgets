a03b42f6da3d7c507d416bc71b5a2c6b4e1fa573
Yevhen Kyivskyi <yevhen.kyivskyi@glia.com>
2024-12-05 18:01:15 +0100

    Improve mark as read SC messages

--- GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.TranscriptModel.LeaveConversation.swift
+++ GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.TranscriptModel.LeaveConversation.swift
@@ -3,9 +3,19 @@ import Foundation
 extension SecureConversations.TranscriptModel {
     func showLeaveConversationDialogIfNeeded() {
         if environment.shouldShowLeaveSecureConversationDialog {
-            engagementAction?(.showAlert(.leaveCurrentConversation { [weak self] in
-                self?.environment.leaveCurrentSecureConversation()
-            }))
+            let action = EngagementViewModel.Action.showAlert(
+                .leaveCurrentConversation(
+                    confirmed: { [weak self] in
+                        self?.environment.leaveCurrentSecureConversation()
+                    }, declined: { [weak self] in
+                        guard let self else {
+                            return
+                        }
+                        self.markMessagesAsRead(with: self.hasUnreadMessages)
+                    }
+                )
+            )
+            engagementAction?(action)
         }
     }
 }
