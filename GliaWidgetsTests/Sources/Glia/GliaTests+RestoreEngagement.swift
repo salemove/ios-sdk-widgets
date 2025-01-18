202fd85d6f7fc60211049b60893398d0c71eea23
Egor Egorov <egorov.egor@glia.com>
2025-01-18 16:18:51 +0200

    Restore bubble when SC Live Upgrade happens
    This commit introduces:
    - the logic for restoring the bubble for the cases when SC engagement gets:
    	- restored from Follow Up;
    	- started using Outbound Message feature;
    	- started while visitor is on SecureConversation.Confirmation screen
    - unit tests
    
    MOB-3951

--- GliaWidgetsTests/Sources/Glia/GliaTests+RestoreEngagement.swift
+++ GliaWidgetsTests/Sources/Glia/GliaTests+RestoreEngagement.swift
@@ -68,4 +68,65 @@ extension GliaTests {
         try XCTAssertTrue(XCTUnwrap(sdk.interactor?.skipLiveObservationConfirmations))
         XCTAssertEqual(calls, [.snackBarPresent])
     }
+
+    func test_restoreOngoingSecureConversationEngagement() throws {
+        var sdkEnv = Glia.Environment.failing
+        sdkEnv.coreSDKConfigurator.configureWithInteractor = { _ in }
+        sdkEnv.createRootCoordinator = { _, _, _, engagementLaunching, _, _, _ in
+            EngagementCoordinator.mock(
+                engagementLaunching: engagementLaunching,
+                screenShareHandler: .mock,
+                environment: .engagementCoordEnvironmentWithKeyWindow
+            )
+        }
+        sdkEnv.print.printClosure = { _, _, _ in }
+        var logger = CoreSdkClient.Logger.failing
+        logger.configureLocalLogLevelClosure = { _ in }
+        logger.configureRemoteLogLevelClosure = { _ in }
+        logger.prefixedClosure = { _ in logger }
+        logger.infoClosure = { _, _, _, _ in }
+        sdkEnv.coreSdk.createLogger = { _ in logger }
+        let siteMock = try CoreSdkClient.Site.mock()
+        sdkEnv.coreSdk.fetchSiteConfigurations = { callback in callback(.success(siteMock)) }
+        sdkEnv.coreSdk.pendingSecureConversationStatus = { _ in }
+        sdkEnv.coreSdk.getSecureUnreadMessageCount = { $0(.success(0)) }
+        sdkEnv.conditionalCompilation.isDebug = { true }
+        sdkEnv.coreSDKConfigurator.configureWithConfiguration = { _, completion in
+            completion(.success(()))
+        }
+        let uuidGen = UUID.incrementing
+        sdkEnv.coreSdk.subscribeForUnreadSCMessageCount = { _ in uuidGen().uuidString }
+        sdkEnv.coreSdk.observePendingSecureConversationStatus = { _ in uuidGen().uuidString }
+        sdkEnv.coreSdk.unsubscribeFromPendingSecureConversationStatus = { _ in }
+        sdkEnv.coreSdk.unsubscribeFromUnreadCount = { _ in }
+        sdkEnv.gcd.mainQueue.async = { $0() }
+
+        let window = UIWindow(frame: .zero)
+        window.rootViewController = .init()
+        window.makeKeyAndVisible()
+        sdkEnv.uiApplication.windows = { [window] }
+        enum Call {
+            case snackBarPresent
+        }
+
+        var calls: [Call] = []
+        sdkEnv.snackBar.present = { _, _, _, _, _, _, _ in
+            calls.append(.snackBarPresent)
+        }
+
+        let sdk = Glia(environment: sdkEnv)
+        try sdk.configure(with: .mock(), features: .all) { _ in }
+        sdk.environment.coreSdk.getCurrentEngagement = { .mock() }
+        sdk.stringProvidingPhase = .configured { _ in
+            return ""
+        }
+        guard let interactor = sdk.interactor else {
+            XCTFail("Interactor missing")
+            return
+        }
+        interactor.state = .engaged(.mock())
+
+        XCTAssertNotNil(sdk.rootCoordinator?.gliaViewController)
+        XCTAssertEqual(calls, [.snackBarPresent])
+    }
 }
