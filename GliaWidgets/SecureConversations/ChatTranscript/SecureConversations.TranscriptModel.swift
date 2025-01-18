079d5bcb91a6c4020a568d02615ce65eedb7d070
Egor Egorov <egorov.egor@glia.com>
2025-01-18 18:11:39 +0200

    Fix `skipLiveObservationConfirmations` value after restored engagement
    Restoring engagement on configuration step or during authentication turns `skipLiveObservationConfirmations` value to `true` to prevent displaying the dialog. This value never turns back to default value, which is `false` without re-configuring the SDK. This commit fixes it.
    
    MOB-3964

--- GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.TranscriptModel.swift
+++ GliaWidgets/SecureConversations/ChatTranscript/SecureConversations.TranscriptModel.swift
@@ -770,7 +770,9 @@ extension SecureConversations.TranscriptModel {
             ),
             showPoweredBy: false,
             filterSecureConversation: true,
-            mediaTypeSelected: .init(closure: entryWidgetMediaTypeSelected),
+            mediaTypeSelected: .init { [weak self] mediaType in
+                self?.entryWidgetMediaTypeSelected(mediaType)
+            },
             mediaTypeItemsStyle: environment.topBannerItemsStyle
         )
     }
