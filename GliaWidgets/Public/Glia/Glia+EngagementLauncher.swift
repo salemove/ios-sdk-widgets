06724362c15243f3b6b397f4fee3af6a571fc0fe
Egor Egorov <egorov.egor@glia.com>
2025-01-20 14:49:52 +0200

    Fix unexpected capturing in EngagementLauncher
    This commit fixes the issue when Interactor instance and media are captured during EngagementLauncher creation.
    
    MOB-3963

--- GliaWidgets/Public/Glia/Glia+EngagementLauncher.swift
+++ GliaWidgets/Public/Glia/Glia+EngagementLauncher.swift
@@ -9,10 +9,39 @@ extension Glia {
     /// - Returns:
     ///   - `EngagementLauncher` instance.
     public func getEngagementLauncher(queueIds: [String]) throws -> EngagementLauncher {
-        let parameters = try getEngagementParameters(in: queueIds)
+        @discardableResult
+        func getConfiguration() throws -> Configuration {
+            guard let configuration else {
+                throw GliaError.sdkIsNotConfigured
+            }
+            return configuration
+        }
+
+        @discardableResult
+        func getInteractor() throws -> Interactor {
+            guard let interactor else {
+                loggerPhase.logger.prefixed(Self.self).warning("Interactor is missing")
+                throw GliaError.sdkIsNotConfigured
+            }
+            return interactor
+        }
+
+        // In order to align behaviour between platforms,
+        // `GliaError.engagementExists` is no longer thrown,
+        // instead engagement is getting restored.
+        try getConfiguration()
+        try getInteractor()
+
         loggerPhase.logger.info("Returning an Engagement Launcher")
-        return try EngagementLauncher { [weak self] engagementKind, sceneProvider in
-            try self?.resolveEngagementState(
+
+        return EngagementLauncher { [weak self] engagementKind, sceneProvider in
+            guard let self else { return }
+            let parameters = try getEngagementParameters(
+                configuration: getConfiguration(),
+                interactor: getInteractor(),
+                queueIds: queueIds
+            )
+            try self.resolveEngagementState(
                 engagementKind: engagementKind,
                 sceneProvider: sceneProvider,
                 configuration: parameters.configuration,
