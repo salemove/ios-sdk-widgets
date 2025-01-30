284631834d004a39cd58a44ff91d5d7994f49f75
Egor Egorov <egorov.egor@glia.com>
2025-01-30 18:46:27 +0200

    Fix restoring engagement when `hasPendingInteraction` is `true`
    MOB-4003

--- GliaWidgetsTests/Sources/Glia/GliaTests+RestoreEngagement.swift
+++ GliaWidgetsTests/Sources/Glia/GliaTests+RestoreEngagement.swift
@@ -200,4 +200,63 @@ extension GliaTests {
 
         XCTAssertNil(sdk.rootCoordinator?.gliaViewController)
     }
+
+    func test_sdkRestoresMessagingWhenOngoingEngagementExistsAndPendingInteractionIsTrue() throws {
+        var sdkEnv = Glia.Environment.failing
+        sdkEnv.coreSDKConfigurator.configureWithInteractor = { _ in }
+        var launching: EngagementCoordinator.EngagementLaunching?
+        sdkEnv.createRootCoordinator = { _, _, _, engagementLaunching, _, _, _ in
+            launching = engagementLaunching
+            return EngagementCoordinator.mock(
+                engagementLaunching: engagementLaunching,
+                screenShareHandler: .mock,
+                environment: .engagementCoordEnvironmentWithKeyWindow
+            )
+        }
+        sdkEnv.print.printClosure = { _, _, _ in }
+        var logger = CoreSdkClient.Logger.failing
+        logger.configureLocalLogLevelClosure = { _ in }
+        logger.configureRemoteLogLevelClosure = { _ in }
+        logger.prefixedClosure = { _ in logger }
+        logger.infoClosure = { _, _, _, _ in }
+        sdkEnv.coreSdk.createLogger = { _ in logger }
+        let siteMock = try CoreSdkClient.Site.mock()
+        sdkEnv.coreSdk.fetchSiteConfigurations = { callback in callback(.success(siteMock)) }
+        sdkEnv.coreSdk.pendingSecureConversationStatus = { _ in }
+        sdkEnv.coreSdk.getSecureUnreadMessageCount = { $0(.success(0)) }
+        sdkEnv.conditionalCompilation.isDebug = { true }
+        sdkEnv.coreSDKConfigurator.configureWithConfiguration = { _, completion in
+            completion(.success(()))
+        }
+        sdkEnv.snackBar.present = { _, _, _, _, _, _, _ in }
+        let uuidGen = UUID.incrementing
+        sdkEnv.coreSdk.subscribeForUnreadSCMessageCount = { _ in uuidGen().uuidString }
+        sdkEnv.coreSdk.observePendingSecureConversationStatus = { callback in
+            callback(.success(true))
+            return uuidGen().uuidString
+        }
+        sdkEnv.coreSdk.unsubscribeFromPendingSecureConversationStatus = { _ in }
+        sdkEnv.coreSdk.unsubscribeFromUnreadCount = { _ in }
+        sdkEnv.gcd.mainQueue.async = { $0() }
+
+        let window = UIWindow(frame: .zero)
+        window.rootViewController = .init()
+        window.makeKeyAndVisible()
+        sdkEnv.uiApplication.windows = { [window] }
+
+        let sdk = Glia(environment: sdkEnv)
+        try sdk.configure(with: .mock(), features: .all) { _ in }
+        sdk.environment.coreSdk.getCurrentEngagement = { .mock() }
+        sdk.stringProvidingPhase = .configured { _ in
+            return ""
+        }
+        guard let interactor = sdk.interactor else {
+            XCTFail("Interactor missing")
+            return
+        }
+        interactor.state = .engaged(.mock())
+
+        XCTAssertNotNil(sdk.rootCoordinator?.gliaViewController)
+        XCTAssertEqual(launching, .direct(kind: .messaging(.chatTranscript)))
+    }
 }
