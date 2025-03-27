65387ec0883104984d3b861275100688deae3bff
Rasmus Tauts <rasmus.tauts@glia.com>
2025-03-27 15:46:26 +0200

    Rename listQueues to getQueues
    This PR renames listQueues to getQueues, to align iOS with Android.
    The actions of those methods will remain unchanged.
    
    MOB-4200

--- GliaWidgets/Sources/QueuesMonitor/QueuesMonitor.Environment.Mock.swift
+++ GliaWidgets/Sources/QueuesMonitor/QueuesMonitor.Environment.Mock.swift
@@ -2,7 +2,7 @@ import Foundation
 #if DEBUG
 extension QueuesMonitor.Environment {
     static let mock: Self = .init(
-        listQueues: { _ in },
+        getQueues: { _ in },
         subscribeForQueuesUpdates: { _, _ in UUID().uuidString },
         unsubscribeFromUpdates: { _, _ in },
         logger: .mock
--- GliaWidgets/Sources/QueuesMonitor/QueuesMonitor.Environment.swift
+++ GliaWidgets/Sources/QueuesMonitor/QueuesMonitor.Environment.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension QueuesMonitor {
     struct Environment {
-        var listQueues: CoreSdkClient.ListQueues
+        var getQueues: CoreSdkClient.ListQueues
         var subscribeForQueuesUpdates: CoreSdkClient.SubscribeForQueuesUpdates
         var unsubscribeFromUpdates: CoreSdkClient.UnsubscribeFromUpdates
         var logger: CoreSdkClient.Logger
