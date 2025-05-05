13afc96b4c7df340913d378cba0c0050cd487b74
Dmytro Keller <dmytro.keller@glia.com>
2025-05-05 13:55:08 +0300

    fix: RemoteConfig isWhiteLabel property

--- GliaWidgets/Sources/RemoteConfiguration/RemoteConfiguration.swift
+++ GliaWidgets/Sources/RemoteConfiguration/RemoteConfiguration.swift
@@ -13,7 +13,7 @@ public struct RemoteConfiguration: Codable {
     let snackBar: SnackBar?
     let webBrowserScreen: WebView?
     let entryWidget: EntryWidget?
-    let isWhiteLabelApp: Bool?
+    let isWhiteLabel: Bool?
 }
 
 extension RemoteConfiguration {
