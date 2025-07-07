b9d9ab6647ebb02c7c543aa08a666f29af50580b
Egor Egorov <egorov.egor@glia.com>
2025-07-07 18:42:25 +0300

    Align operator placeholder paddings
    This commit updates operator placeholder paddings for:
    - operator message bubble;
    - in-app bubble
    
    MOB-4471

--- SnapshotTests/BubbleViewVoiceOverTests.swift
+++ SnapshotTests/BubbleViewVoiceOverTests.swift
@@ -6,7 +6,7 @@ import XCTest
 final class BubbleViewVoiceOverTests: SnapshotTestCase {
     func test_bubble() {
         let bubble = ViewFactory.mock().makeBubbleView()
-        bubble.frame = .init(origin: .zero, size: .init(width: 50, height: 50))
+        bubble.frame = .init(origin: .zero, size: .init(width: 64, height: 64))
         bubble.assertSnapshot(as: .accessibilityImage)
     }
 }
