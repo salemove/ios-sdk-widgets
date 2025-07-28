ae8c88b7525341bc9b61b7ca779289133e323c46
Rasmus Tauts <rasmus.tauts@glia.com>
2025-07-28 18:31:04 +0300

    Remove requestMediaUpgradeOffer interface

--- GliaWidgets/Sources/CoreSDKClient/CoreSDKClient.Mock.swift
+++ GliaWidgets/Sources/CoreSDKClient/CoreSDKClient.Mock.swift
@@ -16,7 +16,6 @@ extension CoreSdkClient {
         configureWithInteractor: { _ in },
         getQueues: { _ in },
         queueForEngagement: { _, _, _ in },
-        requestMediaUpgradeWithOffer: { _, _ in },
         sendMessagePreview: { _, _ in },
         sendMessageWithMessagePayload: { _, _ in },
         cancelQueueTicket: { _, _ in },
