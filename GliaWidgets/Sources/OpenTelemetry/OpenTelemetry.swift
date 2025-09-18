2a3231f154196614a25cd2928d75d394f00dc0f3
Egor Egorov <egorov.egor@glia.com>
2025-09-18 14:27:30 +0300

    Add telemetry logs for Alert Dialogs
    Added events:
    - dialog_shown;
    - dialog_closed;
    - dialog_button_clicked;
    
    MOB-4703

--- GliaWidgets/Sources/OpenTelemetry/OpenTelemetry.swift
+++ GliaWidgets/Sources/OpenTelemetry/OpenTelemetry.swift
@@ -1,6 +1,9 @@
 import GliaCoreSDK
 
 typealias OpenTelemetry = GliaCoreSDK.OpenTelemetry
+typealias OtelLogEvents = GliaCoreSDK.OtelLogEvents
+typealias OtelAttributes = GliaCoreSDK.OtelAttributes
+typealias OtelAttributeValue = GliaCoreSDK.OtelAttributeValue
 typealias OtelButtonNames = GliaCoreSDK.OtelButtonNames
 typealias OtelDialogNames = GliaCoreSDK.OtelDialogNames
 typealias OtelGvaActionTypes = GliaCoreSDK.OtelGvaActionTypes
