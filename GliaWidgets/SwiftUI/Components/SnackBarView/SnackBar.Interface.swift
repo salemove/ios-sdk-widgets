4582a6de7fd1b4d85ab53d1898c66282a35e3e90
Egor Egorov <egorov.egor@glia.com>
2025-11-19 14:42:24 +0200

    Introduce SnackBar dismissal strategy
    This strategy allows to use:
    - automatic dismissal after 3 sec and introduces;
    - manual one allowing to dismiss a SnackBar on any further event
    
    MOB-4905

--- GliaWidgets/SwiftUI/Components/SnackBarView/SnackBar.Interface.swift
+++ GliaWidgets/SwiftUI/Components/SnackBarView/SnackBar.Interface.swift
@@ -4,6 +4,7 @@ struct SnackBar {
     var present: (
         _ text: String,
         _ style: Theme.SnackBarStyle,
+        _ dismissTiming: SnackBar.DismissTiming,
         _ viewController: UIViewController,
         _ bottomOffset: CGFloat,
         _ timerProviding: FoundationBased.Timer.Providing,
@@ -14,6 +15,7 @@ struct SnackBar {
     func present(
         text: String,
         style: Theme.SnackBarStyle,
+        dismissTiming: SnackBar.DismissTiming = .default,
         for viewController: UIViewController,
         bottomOffset: CGFloat = 0,
         timerProviding: FoundationBased.Timer.Providing,
@@ -23,6 +25,7 @@ struct SnackBar {
         present(
             text,
             style,
+            dismissTiming,
             viewController,
             bottomOffset,
             timerProviding,
@@ -34,6 +37,7 @@ struct SnackBar {
     func showSnackBarMessage(
         text: String,
         style: Theme.SnackBarStyle,
+        dismissTiming: SnackBar.DismissTiming = .default,
         topMostViewController: UIViewController,
         timerProviding: FoundationBased.Timer.Providing,
         gcd: GCD,
@@ -42,6 +46,7 @@ struct SnackBar {
         self.present(
             text: text,
             style: style,
+            dismissTiming: dismissTiming,
             for: topMostViewController,
             bottomOffset: -60,
             timerProviding: timerProviding,
