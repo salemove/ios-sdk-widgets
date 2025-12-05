9d9817b4b36896467a4e25502734b1acca64dda8
Egor Egorov <egorov.egor@glia.com>
2025-12-05 17:19:36 +0200

    Network state observation on VideoCall screen
    - added subscribing on network state changes on CallVisualizer.VideoCall screen;
    - "No connection" snackBar displays when `.disconnected` network state arrives;
    - "No connection" snackBar disappears when `.connected` network state arrives;
    
    MOB-4889

--- GliaWidgets/Sources/CallVisualizer/VideoCall/ViewModel/VideoCallViewModel.DelegateEvent.swift
+++ GliaWidgets/Sources/CallVisualizer/VideoCall/ViewModel/VideoCallViewModel.DelegateEvent.swift
@@ -4,5 +4,6 @@ extension CallVisualizer.VideoCallViewModel {
     enum DelegateEvent {
         case propsUpdated(Props)
         case minimized
+        case showSnackBarView(dismissTiming: SnackBar.DismissTiming, style: Theme.SnackBarStyle)
     }
 }
