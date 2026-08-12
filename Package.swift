3019c69626e6f1466d9108cea0afcef2864f33cc
BitriseBot <bitrise@glia.com>
2026-08-12 13:38:33 +0000

    Update dependencies declared in `Podfile` and `Package.swift`

--- Package.swift
+++ Package.swift
@@ -30,13 +30,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/149.0.0/WebRTC-M149.xcframework.zip",
-            checksum: "79c5a3e49a68de30a99baabaf5b4c0067dd7a0b66fdd4b8afb8ec337e746abba"
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.33.0/WebRTC.xcframework.zip",
+            checksum: "f76e410f608d96989ba0312e099697703a37b4414f8f46bb9e30c3d9b4291a52"
         ),
         .binaryTarget(
             name: "GliaCoreSDK",
-            url: "https://github.com/salemove/ios-bundle/releases/download/2.6.12/GliaCoreSDK.xcframework.zip",
-            checksum: "3091164c34ad87357c5d97d355c7015d038f28974175ae1821b55c84383f3cc3"
+            url: "https://github.com/salemove/ios-bundle/releases/download/2.6.13/GliaCoreSDK.xcframework.zip",
+            checksum: "e6754e906fc2b1a97a245b85761ea57e1bc5a672e2bb13f877f928864fdc2485"
         ),
         .binaryTarget(
             name: "GliaWidgetsSDKXcf",
